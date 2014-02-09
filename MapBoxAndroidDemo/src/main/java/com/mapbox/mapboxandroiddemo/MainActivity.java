1b750f0aa8c84dd1644ac7fedd7b5e147dbebf99
Brad Leege <bleege@gmail.com>
2014-02-09 12:23:57 -0600

    Adding existing MapBox demo source code and making it run off the MapBox Android SDK AAR file found in a local Maven Repository

--- MapBoxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
+++ MapBoxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
@@ -1,65 +1,137 @@
 package com.mapbox.mapboxandroiddemo;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.Window;
+import android.widget.Button;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.overlay.Icon;
+import com.mapbox.mapboxsdk.overlay.Marker;
+import com.mapbox.mapboxsdk.overlay.PathOverlay;
+import com.mapbox.mapboxsdk.overlay.mylocation.MyLocationNewOverlay;
+import com.mapbox.mapboxsdk.tileprovider.tilesource.mapboxTileLayer;
+import com.mapbox.mapboxsdk.views.MapController;
+import com.mapbox.mapboxsdk.views.MapView;
+import com.mapbox.mapboxsdk.views.util.TilesLoadedListener;
 
 public class MainActivity extends ActionBarActivity {
+
+    private MapController mapController;
+    private LatLng startingPoint = new LatLng(51f, 0f);
+    private MapView mv;
+    private MyLocationNewOverlay myLocationOverlay;
+    private Paint paint;
+    private String satellite = "brunosan.map-cyglrrfu";
+    private String street = "examples.map-vyofok3q";
+    private String terrain = "examples.map-zgrqqx0w";
+    private String currentLayer = "terrain";
+    private PathOverlay equator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        mv = (MapView)findViewById(R.id.mapview);
+        mapController = mv.getController();
+        mapController.setCenter(startingPoint);
+        mapController.setZoom(4);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        mv.loadFromGeoJSONURL("https://gist.github.com/fdansv/8541618/raw/09da8aef983c8ffeb814d0a1baa8ecf563555b5d/geojsonpointtest");
+        setButtonListeners();
+        Marker m = new Marker(mv, "Hello", "World", new LatLng(0f, 0f));
+        m.setIcon(new Icon(Icon.Size.l, "bus", "000"));
+        mv.addMarker(m);
+
+        mv.setOnTilesLoadedListener(new TilesLoadedListener() {
+            @Override
+            public boolean onTilesLoaded() {
+                return false;
+            }
+        });
+        mv.setVisibility(View.VISIBLE);
+        equator = new PathOverlay();
+        equator.addPoint(0,-89);
+        equator.addPoint(0, 89);
+        mv.getOverlays().add(equator);
     }
 
+    private void setButtonListeners() {
+        Button satBut = changeButtonTypeface((Button)findViewById(R.id.satbut));
+        satBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!currentLayer.equals("satellite")) {
+                    replaceMapView(satellite);
+                    currentLayer = "satellite";
+                }
+                mv.setMultiTouchControls(false);
+            }
+        });
+        Button terBut = changeButtonTypeface((Button)findViewById(R.id.terbut));
+        terBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!currentLayer.equals("terrain")) {
+                    replaceMapView(terrain);
+                    currentLayer = "terrain";
+                }
+            }
+        });
+        Button strBut = changeButtonTypeface((Button)findViewById(R.id.strbut));
+        strBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!currentLayer.equals("street")) {
+                    replaceMapView(street);
+                    currentLayer = "street";
+                }
+            }
+        });
+    }
+
+    protected void replaceMapView(String layer) {
+        mv.setTileSource(new mapboxTileLayer(layer));
+    }
+
+    private void addLocationOverlay() {
+        // Adds an icon that shows location
+        myLocationOverlay = new MyLocationNewOverlay(this, mv);
+        myLocationOverlay.enableMyLocation();
+        myLocationOverlay.setDrawAccuracyEnabled(true);
+        mv.getOverlays().add(myLocationOverlay);
+    }
+    private void addLine() {
+        // Configures a line
+        PathOverlay po = new PathOverlay(Color.RED, this);
+        Paint linePaint = new Paint();
+        linePaint.setStyle(Paint.Style.STROKE);
+        linePaint.setColor(Color.BLUE);
+        linePaint.setStrokeWidth(5);
+        po.setPaint(linePaint);
+        po.addPoint(startingPoint);
+        po.addPoint(new LatLng(51.7, 0.3));
+        po.addPoint(new LatLng(51.2, 0));
+
+        // Adds line and marker to the overlay
+        mv.getOverlays().add(po);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+
         return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
+    private Button changeButtonTypeface(Button button){
+        //Typeface tf = Typeface.createFromAsset(this.getAssets(), "fonts/semibold.ttf");
+        //button.setTypeface(tf);
+        return button;
     }
 
 }
