f0825f327f77487c7a0a944d5b901b4211a82bd6
Brad Leege <bleege@gmail.com>
2015-10-22 17:57:31 -0700

    #35 - Mapbox Android API Conversion

--- MapBoxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
+++ MapBoxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
@@ -7,105 +7,102 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
-import android.text.TextUtils;
+import android.support.v7.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import com.crashlytics.android.Crashlytics;
-import com.mapbox.mapboxsdk.api.ILatLng;
-import com.mapbox.mapboxsdk.geometry.BoundingBox;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.constants.Style;
 import com.mapbox.mapboxsdk.geometry.LatLng;
-import com.mapbox.mapboxsdk.overlay.Icon;
-import com.mapbox.mapboxsdk.overlay.Marker;
-import com.mapbox.mapboxsdk.overlay.UserLocationOverlay;
-import com.mapbox.mapboxsdk.tileprovider.tilesource.*;
 import com.mapbox.mapboxsdk.views.MapView;
-import com.mapbox.mapboxsdk.views.util.TilesLoadedListener;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends ActionBarActivity {
 
     private MapView mv;
-	private UserLocationOverlay myLocationOverlay;
-	private String currentMap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Crashlytics.start(this);
+        Fabric.with(this, new Crashlytics());
 
         setContentView(R.layout.activity_main);
 
         mv = (MapView) findViewById(R.id.mapview);
-		mv.setMinZoomLevel(mv.getTileProvider().getMinimumZoomLevel());
-		mv.setMaxZoomLevel(mv.getTileProvider().getMaximumZoomLevel());
-		mv.setCenter(mv.getTileProvider().getCenterCoordinate());
-		mv.setZoom(0);
-		currentMap = getString(R.string.streetMapId);
+        mv.setStyle(Style.MAPBOX_STREETS);
+        mv.setCenterCoordinate(new LatLng(0, 0));
 
 		// Show user location (purposely not in follow mode)
-        mv.setUserLocationEnabled(true);
+        mv.setMyLocationEnabled(true);
 
-		mv.loadFromGeoJSONURL("https://gist.githubusercontent.com/tmcw/10307131/raw/21c0a20312a2833afeee3b46028c3ed0e9756d4c/map.geojson");
+//		mv.loadFromGeoJSONURL("https://gist.githubusercontent.com/tmcw/10307131/raw/21c0a20312a2833afeee3b46028c3ed0e9756d4c/map.geojson");
 //        setButtonListeners();
-        Marker m = new Marker(mv, "Edinburgh", "Scotland", new LatLng(55.94629, -3.20777));
-        m.setIcon(new Icon(this, Icon.Size.SMALL, "marker-stroked", "ee8a65"));
-        mv.addMarker(m);
-
-        m = new Marker(mv, "Stockholm", "Sweden", new LatLng(59.32995, 18.06461));
-        m.setIcon(new Icon(this, Icon.Size.LARGE, "city", "3887be"));
-        mv.addMarker(m);
-
-        m = new Marker(mv, "Prague", "Czech Republic", new LatLng(50.08734, 14.42112));
-        m.setIcon(new Icon(this, Icon.Size.MEDIUM, "land-use", "3bb2d0"));
-        mv.addMarker(m);
-
-        m = new Marker(mv, "Athens", "Greece", new LatLng(37.97885, 23.71399));
-        mv.addMarker(m);
-
-        m = new Marker(mv, "Tokyo", "Japan", new LatLng(35.70247, 139.71588));
-        m.setIcon(new Icon(this, Icon.Size.LARGE, "city", "3887be"));
-        mv.addMarker(m);
-
-        m = new Marker(mv, "Ayacucho", "Peru", new LatLng(-13.16658, -74.21608));
-        m.setIcon(new Icon(this, Icon.Size.LARGE, "city", "3887be"));
-        mv.addMarker(m);
-
-        m = new Marker(mv, "Nairobi", "Kenya", new LatLng(-1.26676, 36.83372));
-        m.setIcon(new Icon(this, Icon.Size.LARGE, "city", "3887be"));
-        mv.addMarker(m);
-
-        m = new Marker(mv, "Canberra", "Australia", new LatLng(-35.30952, 149.12430));
-        m.setIcon(new Icon(this, Icon.Size.LARGE, "city", "3887be"));
-        mv.addMarker(m);
-
-        mv.setOnTilesLoadedListener(new TilesLoadedListener() {
-            @Override
-            public boolean onTilesLoaded() {
-                return false;
-            }
-
-            @Override
-            public boolean onTilesLoadStarted() {
-                // TODO Auto-generated method stub
-                return false;
-            }
-        });
-//        mv.setVisibility(View.VISIBLE);
+        mv.addMarker(new MarkerOptions().title("Edinburgh").snippet("Scotland").position(new LatLng(55.94629, -3.20777)));
+        mv.addMarker(new MarkerOptions().title("Stockholm").snippet("Sweden").position(new LatLng(59.32995, 18.06461)));
+        mv.addMarker(new MarkerOptions().title("Prague").snippet("Czech Republic").position(new LatLng(50.08734, 14.42112)));
+        mv.addMarker(new MarkerOptions().title("Athens").snippet("Greece").position(new LatLng(37.97885, 23.71399)));
+        mv.addMarker(new MarkerOptions().title("Tokyo").snippet("Japan").position(new LatLng(35.70247, 139.71588)));
+        mv.addMarker(new MarkerOptions().title("Ayacucho").snippet("Peru").position(new LatLng(-13.16658, -74.21608)));
+        mv.addMarker(new MarkerOptions().title("Nairobi").snippet("Kenya").position(new LatLng(-1.26676, 36.83372)));
+        mv.addMarker(new MarkerOptions().title("Canberra").snippet("Australia").position(new LatLng(-35.30952, 149.12430)));
 
 		Button bugsBut = changeButtonTypeface((Button) findViewById(R.id.bugsButton));
 		bugsBut.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				String url = "https://github.com/mapbox/mapbox-android-sdk/issues?state=open";
+				String url = "https://github.com/mapbox/mapbox-gl-native/issues?state=open";
 				Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
 				startActivity(i);
 			}
 		});
+        mv.onCreate(savedInstanceState);
 	}
 
-	@Override
+
+    /**
+     * Dispatch onStart() to all fragments.  Ensure any created loaders are
+     * now started.
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mv.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mv.onDestroy();
+    }
+
+    /**
+     * Dispatch onResume() to fragments.  Note that for better inter-operation
+     * with older versions of the platform, at the point of this call the
+     * fragments attached to the activity are <em>not</em> resumed.  This means
+     * that in some cases the previous state may still be saved, not allowing
+     * fragment transactions that modify the state.  To correctly interact
+     * with fragments in their proper state, you should instead override
+     * {@link #onResumeFragments()}.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mv.onResume();
+    }
+
+    /**
+     * Dispatch onPause() to fragments.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mv.onPause();
+    }
+
+    @Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
 		MenuInflater menuInflater = getMenuInflater();
@@ -118,11 +115,12 @@ public class MainActivity extends ActionBarActivity {
 	{
 		switch (item.getItemId()) {
 			case R.id.menuItemStreets:
-				replaceMapView(getString(R.string.streetMapId));
+                mv.setStyle(Style.MAPBOX_STREETS);
 				return true;
 			case R.id.menuItemSatellite:
-				replaceMapView(getString(R.string.satelliteMapId));
+                mv.setStyle(Style.SATELLITE);
 				return true;
+/*
 			case R.id.menuItemTerrain:
 				replaceMapView(getString(R.string.terrainMapId));
 				return true;
@@ -138,44 +136,22 @@ public class MainActivity extends ActionBarActivity {
 			case R.id.menuItemSpaceship:
 				replaceMapView(getString(R.string.spaceShipMapId));
 				return true;
+*/
 			default:
 				return super.onOptionsItemSelected(item);
 		}
 	}
-
-    protected void replaceMapView(String layer) {
-
-		if (TextUtils.isEmpty(layer) || TextUtils.isEmpty(currentMap) || currentMap.equalsIgnoreCase(layer)) {
-			return;
-		}
-
-        ITileLayer source;
-        BoundingBox box;
-
-        source = new MapboxTileLayer(layer);
-
-        mv.setTileSource(source);
-        box = source.getBoundingBox();
-        mv.setScrollableAreaLimit(box);
-        mv.setMinZoomLevel(mv.getTileProvider().getMinimumZoomLevel());
-        mv.setMaxZoomLevel(mv.getTileProvider().getMaximumZoomLevel());
-		currentMap = layer;
-/*
-        mv.setCenter(mv.getTileProvider().getCenterCoordinate());
-        mv.setZoom(0);
-*/
-    }
 
     private Button changeButtonTypeface(Button button) {
         return button;
     }
 
     public LatLng getMapCenter() {
-        return mv.getCenter();
+        return mv.getCenterCoordinate();
     }
 
-    public void setMapCenter(ILatLng center) {
-        mv.setCenter(center);
+    public void setMapCenter(LatLng center) {
+        mv.setCenterCoordinate(center);
     }
 
     /**
