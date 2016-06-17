a68b8e15a992a77437c39a65a1daa6e129b9127c
Cameron <cameron@mapbox.com>
2016-06-17 18:08:37 -0400

    temp folder added

--- temp/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
+++ temp/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
@@ -116,7 +116,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             if (drawer.isDrawerOpen(GravityCompat.START)) {
                 drawer.closeDrawer(GravityCompat.START);
             } else {
-                super.onBackPressed();
+                moveTaskToBack(true);
             }
         }
     }
