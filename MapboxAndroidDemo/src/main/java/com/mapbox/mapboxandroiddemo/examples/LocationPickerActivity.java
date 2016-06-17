304afc607d5cb92beeac18882a6503d4f9a5cc48
Cameron <cameron@mapbox.com>
2016-06-17 18:16:58 -0400

    removed unnecessary dependency in location picker example

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/LocationPickerActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/LocationPickerActivity.java
@@ -6,7 +6,6 @@ import android.graphics.PointF;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
