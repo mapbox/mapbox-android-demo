190a23d21f708dd2067136518561555e37a7ce71
Cameron <cameron@mapbox.com>
2016-06-27 15:25:05 -0400

    commented and cleaned up code

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/CustomInfoWindowActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/CustomInfoWindowActivity.java
@@ -1,24 +1,16 @@
 package com.mapbox.mapboxandroiddemo.examples;
 
-import android.content.Context;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.mapbox.mapboxandroiddemo.R;
-import com.mapbox.mapboxsdk.annotations.Icon;
-import com.mapbox.mapboxsdk.annotations.IconFactory;
 import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng;
@@ -61,32 +53,44 @@ public class CustomInfoWindowActivity extends AppCompatActivity {
                     @Override
                     public View getInfoWindow(@NonNull Marker marker) {
 
+                        // The info window layout is created dynamically, parent is the info window
+                        // container
                         LinearLayout parent = new LinearLayout(CustomInfoWindowActivity.this);
-
-                        parent.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                        parent.setLayoutParams(new LinearLayout.LayoutParams(
+                                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                         parent.setOrientation(LinearLayout.VERTICAL);
 
+                        // Depending on the marker title, the correct image source is used. If you
+                        // have many markers using different images, extending Marker and
+                        // baseMarkerOptions, adding additional options such as the image, might be
+                        // a better choice.
                         ImageView countryFlagImage = new ImageView(CustomInfoWindowActivity.this);
-                        switch(marker.getTitle()){
+                        switch (marker.getTitle()) {
                             case "spain":
-                                countryFlagImage.setImageDrawable(ContextCompat.getDrawable(CustomInfoWindowActivity.this, R.drawable.flag_of_spain));
+                                countryFlagImage.setImageDrawable(ContextCompat.getDrawable(
+                                        CustomInfoWindowActivity.this, R.drawable.flag_of_spain));
                                 break;
                             case "egypt":
-                                countryFlagImage.setImageDrawable(ContextCompat.getDrawable(CustomInfoWindowActivity.this, R.drawable.flag_of_egypt));
+                                countryFlagImage.setImageDrawable(ContextCompat.getDrawable(
+                                        CustomInfoWindowActivity.this, R.drawable.flag_of_egypt));
                                 break;
                             default:
-                                countryFlagImage.setImageDrawable(ContextCompat.getDrawable(CustomInfoWindowActivity.this, R.drawable.flag_of_germany));
+                                // By default all markers without a matching title will use the
+                                // Germany flag
+                                countryFlagImage.setImageDrawable(ContextCompat.getDrawable(
+                                        CustomInfoWindowActivity.this, R.drawable.flag_of_germany));
                                 break;
                         }
 
+                        // Set the size of the image
                         countryFlagImage.setLayoutParams(new android.view.ViewGroup.LayoutParams(150, 100));
 
+                        // add the image view to the parent layout
                         parent.addView(countryFlagImage);
 
                         return parent;
                     }
                 });
-
             }
         });
     }
@@ -120,5 +124,4 @@ public class CustomInfoWindowActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
     }
-
 }
