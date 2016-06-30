90b71230c7a97c0d13d115971da34c10a7e65565
Cameron <cameron@mapbox.com>
2016-06-30 17:46:03 -0400

    added icon and marker rotation

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/labs/SpaceStationLocationActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/labs/SpaceStationLocationActivity.java
@@ -3,14 +3,19 @@ package com.mapbox.mapboxandroiddemo.labs;
 import android.animation.ObjectAnimator;
 import android.animation.TypeEvaluator;
 import android.animation.ValueAnimator;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.animation.LinearInterpolator;
 
 import com.mapbox.mapboxandroiddemo.R;
 import com.mapbox.mapboxandroiddemo.model.IssModel;
+import com.mapbox.mapboxsdk.annotations.Icon;
+import com.mapbox.mapboxsdk.annotations.IconFactory;
 import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerView;
 import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
@@ -22,6 +27,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
 
 public class SpaceStationLocationActivity extends AppCompatActivity{
 
@@ -29,7 +35,7 @@ public class SpaceStationLocationActivity extends AppCompatActivity{
     private Handler handler;
     private Runnable runnable;
 
-    private Marker marker;
+    private MarkerView marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +53,7 @@ public class SpaceStationLocationActivity extends AppCompatActivity{
                         .addConverterFactory(GsonConverterFactory.create())
                         .build();
 
-                final IssAPI service = client.create(IssAPI.class);
+                final IssAPIService service = client.create(IssAPIService.class);
 
                 final int delay = 2000;
                 handler = new Handler();
@@ -64,9 +70,15 @@ public class SpaceStationLocationActivity extends AppCompatActivity{
                                 double longitude = response.body().getIssPosition().getLongitude();
 
                                 if(marker == null){
-                                    marker = mapboxMap.addMarker(new MarkerViewOptions().position(new LatLng(latitude, longitude)));
+                                    IconFactory iconFactory = IconFactory.getInstance(SpaceStationLocationActivity.this);
+                                    Drawable iconDrawable = ContextCompat.getDrawable(SpaceStationLocationActivity.this, R.drawable.iss);
+                                    Icon icon = iconFactory.fromDrawable(iconDrawable);
+
+                                    marker = mapboxMap.addMarker(new MarkerViewOptions().position(new LatLng(latitude, longitude)).icon(icon));
                                     return;
                                 }
+
+                                marker.setRotation((float) computeHeading(marker.getPosition(), new LatLng(latitude, longitude)));
 
                                 ValueAnimator markerAnimator = ObjectAnimator.ofObject(marker, "position",
                                         new LatLngEvaluator(), marker.getPosition(), new LatLng(latitude, longitude));
@@ -144,5 +156,29 @@ public class SpaceStationLocationActivity extends AppCompatActivity{
                     ((endValue.getLongitude() - startValue.getLongitude()) * fraction));
             return latLng;
         }
+    }
+
+    /**
+     * Returns the heading from one LatLng to another LatLng. Headings are
+     * expressed in degrees clockwise from North within the range [-180,180).
+     * @return The heading in degrees clockwise from north.
+     * <p/>
+     * http://williams.best.vwh.net/avform.htm#Crs
+     */
+    public static double computeHeading(LatLng from, LatLng to) {
+        double fromLat = Math.toRadians(from.getLatitude());
+        double fromLng = Math.toRadians(from.getLongitude());
+        double toLat = Math.toRadians(to.getLatitude());
+        double toLng = Math.toRadians(to.getLongitude());
+        double dLng = toLng - fromLng;
+        double heading = Math.atan2(Math.sin(dLng) * Math.cos(toLat),
+                Math.cos(fromLat) * Math.sin(toLat) - Math.sin(fromLat) * Math.cos(toLat) * Math.cos(dLng));
+        return (Math.toDegrees(heading) >= -180 && Math.toDegrees(heading) < 180) ?
+                Math.toDegrees(heading) : ((((Math.toDegrees(heading) + 180) % 360) + 360) % 360 + -180);
+    }
+
+    public interface IssAPIService {
+        @GET("iss-now")
+        Call<IssModel> loadLocation();
     }
 }
