8a85d8a867fd92bbc4d55526acf40403cdb4f1f4
Cameron <cameron@mapbox.com>
2016-08-16 10:13:26 -0400

    added example showing customization of user location dot

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
@@ -35,6 +35,7 @@ import com.mapbox.mapboxandroiddemo.examples.camera.AnimateMapCameraActivity;
 import com.mapbox.mapboxandroiddemo.examples.camera.BoundingBoxCameraActivity;
 import com.mapbox.mapboxandroiddemo.examples.location.AnimatedLocationIconActivity;
 import com.mapbox.mapboxandroiddemo.examples.location.BasicUserLocation;
+import com.mapbox.mapboxandroiddemo.examples.location.CustomizeUserLocationActivity;
 import com.mapbox.mapboxandroiddemo.examples.location.LocationTrackingActivity;
 import com.mapbox.mapboxandroiddemo.examples.mas.DirectionsActivity;
 import com.mapbox.mapboxandroiddemo.examples.mas.GeocodingActivity;
@@ -214,6 +215,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
             case R.id.nav_location:
                 exampleItemModel.add(new ExampleItemModel(R.string.activity_location_animated_icon_title, R.string.activity_location_animated_icon_description, new Intent(MainActivity.this, AnimatedLocationIconActivity.class), R.string.activity_location_animated_icon_url, true));
+                exampleItemModel.add(new ExampleItemModel(R.string.activity_location_customize_user_title, R.string.activity_location_customize_user_description, new Intent(MainActivity.this, CustomizeUserLocationActivity.class), R.string.empty_image, true));
                 exampleItemModel.add(new ExampleItemModel(R.string.activity_location_basic_title, R.string.activity_location_basic_description, new Intent(MainActivity.this, BasicUserLocation.class), R.string.activity_location_basic_image_url));
                 exampleItemModel.add(new ExampleItemModel(R.string.activity_location_tracking_title, R.string.activity_location_tracking_description, new Intent(MainActivity.this, LocationTrackingActivity.class), R.string.activity_location_tracking_url));
                 currentCategory = R.id.nav_location;
