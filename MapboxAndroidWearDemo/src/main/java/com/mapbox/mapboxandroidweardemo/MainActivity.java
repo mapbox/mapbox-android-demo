9d99ad5250bf229d95f7f58787d7f7478930988d
Antonio Zugaldia <antonio@zugaldia.net>
2016-09-24 16:18:31 -0400

    adds wear module

---
+++ MapboxAndroidWearDemo/src/main/java/com/mapbox/mapboxandroidweardemo/MainActivity.java
@@ -0,0 +1,83 @@
+package com.mapbox.mapboxandroidweardemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+
+import com.mapbox.mapboxsdk.MapboxAccountManager;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
+public class MainActivity extends Activity {
+
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Mapbox access token is configured here. This needs to be called either in your application
+        // object or in the same activity which contains the mapview.
+        MapboxAccountManager.start(this, getString(R.string.access_token));
+
+        setContentView(R.layout.activity_main);
+
+        WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new MapboxOnLayoutInflatedListener(savedInstanceState));
+    }
+
+    class MapboxOnLayoutInflatedListener implements WatchViewStub.OnLayoutInflatedListener {
+
+        private Bundle savedInstanceState;
+
+        public MapboxOnLayoutInflatedListener(Bundle savedInstanceState) {
+            this.savedInstanceState = savedInstanceState;
+        }
+
+        @Override
+        public void onLayoutInflated(WatchViewStub watchViewStub) {
+            mapView = (MapView) findViewById(R.id.mapView);
+            mapView.onCreate(savedInstanceState);
+            mapView.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(MapboxMap mapboxMap) {
+
+                    // Customize map with markers, polylines, etc.
+
+                }
+            });
+        }
+    }
+
+    // Add the mapView lifecycle to the activity's lifecycle methods
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+}
