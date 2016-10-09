f009a168290bbe75346f840340047512bf8b37ac
Cameron <cameron@mapbox.com>
2016-10-09 14:51:46 -0400

    removed utils folder and offsetting helper

--- MapboxAndroidWearDemo/src/main/java/com/mapbox/mapboxandroidweardemo/MainActivity.java
+++ MapboxAndroidWearDemo/src/main/java/com/mapbox/mapboxandroidweardemo/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.WearableRecyclerView;
 
-import com.mapbox.mapboxandroidweardemo.utils.MyOffsettingHelper;
 import com.mapbox.mapboxandroidweardemo.adapter.ExampleAdapter;
 import com.mapbox.mapboxandroidweardemo.examples.SimpleMapViewActivity;
 import com.mapbox.mapboxandroidweardemo.model.ExampleItemModel;
@@ -28,9 +27,9 @@ public class MainActivity extends WearableActivity implements ExampleAdapter.Ite
     wearableRecyclerView.setHasFixedSize(true);
     wearableRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-    MyOffsettingHelper myOffsettingHelper = new MyOffsettingHelper();
-
-    wearableRecyclerView.setOffsettingHelper(myOffsettingHelper);
+//    MyOffsettingHelper myOffsettingHelper = new MyOffsettingHelper();
+//
+//    wearableRecyclerView.setOffsettingHelper(myOffsettingHelper);
 
     exampleItemModels = new ArrayList<>();
     exampleItemModels.add(new ExampleItemModel(R.string.activity_simple_mapview_title, R.drawable.simple_map_view_screen, new Intent(MainActivity.this, SimpleMapViewActivity.class)));
