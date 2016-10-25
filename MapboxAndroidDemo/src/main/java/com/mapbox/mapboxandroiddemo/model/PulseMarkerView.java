e8d3d61a35349e82799965bca91b0d6c58942681
Cameron <cameron@mapbox.com>
2016-10-25 15:44:53 -0400

    added missing file back

---
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/model/PulseMarkerView.java
@@ -0,0 +1,11 @@
+package com.mapbox.mapboxandroiddemo.model;
+
+import com.mapbox.mapboxsdk.annotations.BaseMarkerViewOptions;
+import com.mapbox.mapboxsdk.annotations.MarkerView;
+
+public class PulseMarkerView extends MarkerView {
+
+  public PulseMarkerView(BaseMarkerViewOptions baseMarkerViewOptions) {
+    super(baseMarkerViewOptions);
+  }
+}
