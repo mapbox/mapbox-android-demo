77ce96176093d66024e8b8da56841612c11e1662
Cameron <cameron@mapbox.com>
2016-10-27 11:24:11 -0400

    finished indoor map example

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/labs/IndoorMapActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/labs/IndoorMapActivity.java
@@ -1,67 +1,45 @@
 package com.mapbox.mapboxandroiddemo.labs;
 
 import android.graphics.Color;
-import android.graphics.PointF;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
-import android.widget.Toast;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.Button;
 
 import com.mapbox.mapboxandroiddemo.R;
 import com.mapbox.mapboxsdk.MapboxAccountManager;
-import com.mapbox.mapboxsdk.annotations.PolygonOptions;
-import com.mapbox.mapboxsdk.annotations.PolylineOptions;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
-import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.style.layers.FillLayer;
 import com.mapbox.mapboxsdk.style.layers.Function;
 import com.mapbox.mapboxsdk.style.layers.LineLayer;
-import com.mapbox.mapboxsdk.style.layers.NoSuchLayerException;
-import com.mapbox.mapboxsdk.style.layers.Property;
-import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
-import com.mapbox.services.commons.geojson.Feature;
-import com.mapbox.services.commons.geojson.FeatureCollection;
-import com.mapbox.services.commons.geojson.LineString;
-import com.mapbox.services.commons.geojson.Polygon;
-import com.mapbox.services.commons.turf.TurfGrids;
-import com.mapbox.services.navigation.v5.RouteUtils;
+import com.mapbox.services.commons.models.Position;
+import com.mapbox.services.commons.turf.TurfException;
+import com.mapbox.services.commons.turf.TurfJoins;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
-import static com.mapbox.mapboxsdk.style.layers.Filter.all;
-import static com.mapbox.mapboxsdk.style.layers.Filter.eq;
 import static com.mapbox.mapboxsdk.style.layers.Function.stop;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillAntialias;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillColor;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillOpacity;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillOutlineColor;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineColor;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineOpacity;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineWidth;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.symbolPlacement;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.textField;
 
 public class IndoorMapActivity extends AppCompatActivity {
 
-  private static final String TAG = "IndoorMapActivity";
+  private GeoJsonSource indoorBuildingSource;
+  private boolean levelButtonsShowing = true;
+  private List<Position> boundingBox;
+  private View levelButtons;
   private MapView mapView;
   private MapboxMap map;
 
@@ -78,69 +56,67 @@ public class IndoorMapActivity extends AppCompatActivity {
       public void onMapReady(final MapboxMap mapboxMap) {
         map = mapboxMap;
 
-        GeoJsonSource indoorBuildingSource = new GeoJsonSource("indoor-building", loadJSONFromAsset("white_house_lvl_0.geojson"));
-        mapboxMap.addSource(indoorBuildingSource);
+        levelButtons = findViewById(R.id.floor_level_buttons);
 
-//        GeoJsonSource mainBuildingSource = new GeoJsonSource("main-building-source", loadJSONFromAsset("college_of_tech.geojson"));
-//        mapboxMap.addSource(mainBuildingSource);
-//
-//        GeoJsonSource mainBuildingSourceLvlTwo = new GeoJsonSource("main-building-source-lvl-two", loadJSONFromAsset("college_of_tech_lvl_one.geojson"));
-//        mapboxMap.addSource(mainBuildingSourceLvlTwo);
+        boundingBox = new ArrayList<>();
+        boundingBox.add(Position.fromCoordinates(-77.03791, 38.89715));
+        boundingBox.add(Position.fromCoordinates(-77.03791, 38.89811));
+        boundingBox.add(Position.fromCoordinates(-77.03532, 38.89811));
+        boundingBox.add(Position.fromCoordinates(-77.03532, 38.89708));
 
-        // Add the building layers since we know zoom levels in range
-        loadBuildingLayers();
-        //loadMainBuildingLvlOneLayer();
-
-        mapboxMap.setOnMapClickListener(new MapboxMap.OnMapClickListener() {
+        mapboxMap.setOnCameraChangeListener(new MapboxMap.OnCameraChangeListener() {
           @Override
-          public void onMapClick(@NonNull LatLng latLng) {
+          public void onCameraChange(CameraPosition position) {
 
-
-            try {
-              mapboxMap.removeLayer("main-building-level-two-fill-layer");
-              mapboxMap.removeLayer("main-building-level-two-line-layer");
-              loadMainBuildingLvlOneLayer();
-            } catch (NoSuchLayerException noSuchLayerException) {
-              noSuchLayerException.printStackTrace();
-            }
-
-            try {
-              mapboxMap.removeLayer("main-building-level-one-fill-layer");
-              mapboxMap.removeLayer("main-building-level-one-line-layer");
-              loadMainBuildingLvlTwoLayer();
-            } catch (NoSuchLayerException noSuchLayerException) {
-              noSuchLayerException.printStackTrace();
-            }
-
-            final PointF point = mapboxMap.getProjection().toScreenLocation(latLng);
-            List<Feature> features = mapboxMap.queryRenderedFeatures(point, "indoor-building-fill");
-
-            //remove layer / source if already added
-            try {
-              mapboxMap.removeSource("highlighted-shapes-source");
-              mapboxMap.removeLayer("highlighted-shapes-layer");
-            } catch (Exception exception) {
-              //that's ok
-            }
-
-            if (mapboxMap.getCameraPosition().zoom > 17) {
-              //Add layer / source
-              mapboxMap.addSource(new GeoJsonSource("highlighted-shapes-source", FeatureCollection.fromFeatures(features)));
-              mapboxMap.addLayer(new FillLayer("highlighted-shapes-layer", "highlighted-shapes-source")
-                .withProperties(fillColor(Color.parseColor("#50667f"))));
-
-              if (features.size() > 0) {
-                if (features.get(0).getProperty("tags").getAsJsonObject().has("ref")) {
-                  Toast.makeText(IndoorMapActivity.this, features.get(0).getProperty("tags").getAsJsonObject().get("ref").toString(), Toast.LENGTH_LONG).show();
+            if (position.zoom > 16) {
+              try {
+                if (TurfJoins.inside(Position.fromCoordinates(
+                  position.target.getLongitude(),
+                  position.target.getLatitude()),
+                  boundingBox)) {
+                  if (!levelButtonsShowing) {
+                    showLevelButton();
+                  }
+                } else {
+                  if (levelButtonsShowing) {
+                    hideLevelButton();
+                  }
                 }
+              } catch (TurfException turfException) {
+                turfException.printStackTrace();
               }
+            } else if (levelButtonsShowing) {
+              hideLevelButton();
             }
           }
         });
+
+        String geojsonString = loadJsonFromAsset("white_house_lvl_0.geojson");
+        if (geojsonString != null) {
+          indoorBuildingSource = new GeoJsonSource("indoor-building", geojsonString);
+          mapboxMap.addSource(indoorBuildingSource);
+
+          // Add the building layers since we know zoom levels in range
+          loadBuildingLayer();
+        }
       }
     });
 
+    Button buttonSecondLevel = (Button) findViewById(R.id.second_level_button);
+    buttonSecondLevel.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        indoorBuildingSource.setGeoJson(loadJsonFromAsset("white_house_lvl_1.geojson"));
+      }
+    });
 
+    Button buttonGroundLevel = (Button) findViewById(R.id.ground_level_button);
+    buttonGroundLevel.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        indoorBuildingSource.setGeoJson(loadJsonFromAsset("white_house_lvl_0.geojson"));
+      }
+    });
   }
 
   @Override
@@ -173,79 +149,40 @@ public class IndoorMapActivity extends AppCompatActivity {
     mapView.onSaveInstanceState(outState);
   }
 
-  private void loadMainBuildingLvlOneLayer() {
-    FillLayer mainBuildingLevelOneFillLayer = new FillLayer("main-building-level-one-fill-layer", "main-building-source");
-
-//    mainBuildingLevelOneFillLayer.setFilter(eq("type", "room"));
-
-    mainBuildingLevelOneFillLayer.setProperties(
-      fillColor(Color.parseColor("#eeeeee")),
-      fillOpacity(Function.zoom(0.8f,
-        stop(18, fillOpacity(1f)),
-        stop(17.5f, fillOpacity(0.5f)),
-        stop(17, fillOpacity(0f))
-      ))
-
-    );
-
-    map.addLayer(mainBuildingLevelOneFillLayer);
-
-    LineLayer mainBuildingLevelOneLineLayer = new LineLayer("main-building-level-one-line-layer", "main-building-source");
-
-    mainBuildingLevelOneLineLayer.setProperties(
-      lineColor(Color.parseColor("#50667f")),
-      lineWidth(0.5f),
-      lineOpacity(Function.zoom(0.8f,
-        stop(18, lineOpacity(1f)),
-        stop(17.5f, lineOpacity(0.5f)),
-        stop(17, lineOpacity(0f))
-      ))
-    );
-
-    map.addLayer(mainBuildingLevelOneLineLayer);
+  private void hideLevelButton() {
+    // When the user moves away from our bounding box region or zooms out far enough the floor level
+    // buttons are faded out and hidden.
+    levelButtonsShowing = false;
+    AlphaAnimation animation = new AlphaAnimation(1.0f, 0.0f);
+    animation.setDuration(500);
+    levelButtons.startAnimation(animation);
+    levelButtons.setVisibility(View.GONE);
   }
 
-  private void loadMainBuildingLvlTwoLayer() {
-    FillLayer mainBuildingLevelTwoFillLayer = new FillLayer("main-building-level-two-fill-layer", "main-building-source-lvl-two");
-
-    mainBuildingLevelTwoFillLayer.setProperties(
-      fillColor(Color.parseColor("#eeeeee")),
-      fillOpacity(Function.zoom(0.8f,
-        stop(18, fillOpacity(1f)),
-        stop(17.5f, fillOpacity(0.5f)),
-        stop(17, fillOpacity(0f))
-      ))
-
-    );
-
-    map.addLayer(mainBuildingLevelTwoFillLayer);
-
-    LineLayer mainBuildingLevelTwoLineLayer = new LineLayer("main-building-level-two-line-layer", "main-building-source-lvl-two");
-
-    mainBuildingLevelTwoLineLayer.setProperties(
-      lineColor(Color.parseColor("#50667f")),
-      lineWidth(0.5f),
-      lineOpacity(Function.zoom(0.8f,
-        stop(18, lineOpacity(1f)),
-        stop(17.5f, lineOpacity(0.5f)),
-        stop(17, lineOpacity(0f))
-      ))
-    );
-
-    map.addLayer(mainBuildingLevelTwoLineLayer);
+  private void showLevelButton() {
+    // When the user moves inside our bounding box region or zooms in to a high enough zoom level,
+    // the floor level buttons are faded out and hidden.
+    levelButtonsShowing = true;
+    AlphaAnimation animation = new AlphaAnimation(0.0f, 1.0f);
+    animation.setDuration(500);
+    levelButtons.startAnimation(animation);
+    levelButtons.setVisibility(View.VISIBLE);
   }
 
-  private void loadBuildingLayers() {
+  private void loadBuildingLayer() {
+    // Method used to load the indoor layer on the map. First the fill layer is drawn and then the
+    // line layer is added.
+
     FillLayer indoorBuildingLayer = new FillLayer("indoor-building-fill", "indoor-building");
-
-    //indoorBuildingLayer.setFilter(eq("type", "room"));
 
     indoorBuildingLayer.setProperties(
       fillColor(Color.parseColor("#eeeeee")),
+      // Function.zoom is used here to fade out the indoor layer if zoom level is beyond 16. Only
+      // necessary to show the indoor map at high zoom levels.
       fillOpacity(Function.zoom(0.8f,
-        stop(18, fillOpacity(1f)),
-        stop(17.5f, fillOpacity(0.5f)),
-        stop(17, fillOpacity(0f))
+        stop(17, fillOpacity(1f)),
+        stop(16.5f, fillOpacity(0.5f)),
+        stop(16, fillOpacity(0f))
       ))
 
     );
@@ -255,54 +192,35 @@ public class IndoorMapActivity extends AppCompatActivity {
     LineLayer indoorBuildingLineLayer = new LineLayer("indoor-building-line", "indoor-building");
 
     indoorBuildingLineLayer.setProperties(
-
       lineColor(Color.parseColor("#50667f")),
       lineWidth(0.5f),
       lineOpacity(Function.zoom(0.8f,
-        stop(18, lineOpacity(1f)),
-        stop(17.5f, lineOpacity(0.5f)),
-        stop(17, lineOpacity(0f))
+        stop(17, lineOpacity(1f)),
+        stop(16.5f, lineOpacity(0.5f)),
+        stop(16, lineOpacity(0f))
       ))
 
     );
 
     map.addLayer(indoorBuildingLineLayer);
-
-
-//    SymbolLayer indoorBuildingSymbolLayer = new SymbolLayer("indoor-building-labels", "indoor-building");
-//
-//    indoorBuildingSymbolLayer.setFilter(eq("type", "way"));
-//
-//    indoorBuildingSymbolLayer.setProperties(
-//      symbolPlacement(Property.SYMBOL_PLACEMENT_POINT),
-//      textField("101")
-//    );
-//
-//    map.addLayer(indoorBuildingSymbolLayer);
   }
 
-  private String loadJSONFromAsset(String filename) {
-    String json = null;
+  private String loadJsonFromAsset(String filename) {
+    // Using this method to load in GeoJSON files from the assets folder.
+
+    String json;
+
     try {
-
       InputStream is = getAssets().open(filename);
-
       int size = is.available();
-
       byte[] buffer = new byte[size];
-
-      is.read(buffer);
-
       is.close();
-
       json = new String(buffer, "UTF-8");
-
 
     } catch (IOException ex) {
       ex.printStackTrace();
       return null;
     }
     return json;
-
   }
 }
