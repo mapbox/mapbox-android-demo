298ad89b33dac84b48f8f2dbfd0fd33b33a0b063
Cameron Mace <cameron@mapbox.com>
2016-12-13 15:58:01 -0500

    Fixes potential memory leak in location examples (#183)
    * fixes 180
    
    * removed unused imports

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/location/CustomizeUserLocationActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/location/CustomizeUserLocationActivity.java
@@ -3,7 +3,6 @@ package com.mapbox.mapboxandroiddemo.examples.location;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -11,10 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.mapbox.mapboxandroiddemo.R;
 import com.mapbox.mapboxsdk.MapboxAccountManager;
-import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.constants.MyLocationTracking;
-import com.mapbox.mapboxsdk.geometry.LatLng;
-import com.mapbox.mapboxsdk.location.LocationListener;
 import com.mapbox.mapboxsdk.location.LocationServices;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
@@ -49,10 +45,6 @@ public class CustomizeUserLocationActivity extends AppCompatActivity {
         map = mapboxMap;
 
         enableGps();
-
-        // Enable user tracking to show the padding affect.
-        map.getTrackingSettings().setMyLocationTrackingMode(MyLocationTracking.TRACKING_FOLLOW);
-        map.getTrackingSettings().setDismissAllTrackingOnGesture(false);
 
         // Customize the user location icon using the getMyLocationViewSettings object.
         map.getMyLocationViewSettings().setPadding(0, 500, 0, 0);
@@ -96,7 +88,7 @@ public class CustomizeUserLocationActivity extends AppCompatActivity {
   private void enableGps() {
     // Check if user has granted location permission
     if (!locationServices.areLocationPermissionsGranted()) {
-      ActivityCompat.requestPermissions(this, new String[]{
+      ActivityCompat.requestPermissions(this, new String[] {
         Manifest.permission.ACCESS_COARSE_LOCATION,
         Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_LOCATION);
     } else {
@@ -105,27 +97,9 @@ public class CustomizeUserLocationActivity extends AppCompatActivity {
   }
 
   private void enableLocation() {
-    // If we have the last location of the user, we can move the camera to that position.
-    Location lastLocation = locationServices.getLastLocation();
-    if (lastLocation != null) {
-      map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lastLocation), 16));
-    }
-
-    locationServices.addLocationListener(new LocationListener() {
-      @Override
-      public void onLocationChanged(Location location) {
-        if (location != null) {
-          // Move the map camera to where the user location is and then remove the
-          // listener so the camera isn't constantly updating when the user location
-          // changes. When the user disables and then enables the location again, this
-          // listener is registered again and will adjust the camera once again.
-          map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location), 16));
-          locationServices.removeLocationListener(this);
-        }
-      }
-    });
-    // Enable or disable the location layer on the map
-    map.setMyLocationEnabled(true);
+    // Enable user tracking to show the padding affect.
+    map.getTrackingSettings().setMyLocationTrackingMode(MyLocationTracking.TRACKING_FOLLOW);
+    map.getTrackingSettings().setDismissAllTrackingOnGesture(false);
   }
 
   @Override
