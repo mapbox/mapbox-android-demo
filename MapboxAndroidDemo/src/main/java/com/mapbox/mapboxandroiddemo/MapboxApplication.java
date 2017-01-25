14c8485518aee36efc59d9774febeb4e5e0ef9e1
Tobrun <tobrun.van.nuland@gmail.com>
2017-01-25 19:25:30 +0100

    add firebase crash (#196)

---
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MapboxApplication.java
@@ -0,0 +1,19 @@
+package com.mapbox.mapboxandroiddemo;
+
+import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+public class MapboxApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    FirebaseApp.initializeApp(this, new FirebaseOptions.Builder()
+      .setApiKey(getString(R.string.firebase_api_key))
+      .setApplicationId(getString(R.string.firebase_app_id))
+      .build()
+    );
+  }
+}
