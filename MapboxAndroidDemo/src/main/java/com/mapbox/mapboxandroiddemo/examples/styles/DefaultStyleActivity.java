0e772cea88a66c72568a96b56bc1eccaea58483b
langsmith <langstonlmcs@gmail.com>
2017-02-10 10:45:47 -0500

    added options menu switching between default styles

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/styles/DefaultStyleActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/styles/DefaultStyleActivity.java
@@ -2,6 +2,8 @@ package com.mapbox.mapboxandroiddemo.examples.styles;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.mapbox.mapboxandroiddemo.R;
 import com.mapbox.mapboxsdk.Mapbox;
@@ -11,70 +13,109 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 public class DefaultStyleActivity extends AppCompatActivity {
 
-  private MapView mapView;
+    private MapView mapView;
+    private MapboxMap map;
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    // Mapbox access token is configured here. This needs to be called either in your application
-    // object or in the same activity which contains the mapview.
-    Mapbox.getInstance(this, getString(R.string.access_token));
+        // Mapbox access token is configured here. This needs to be called either in your application
+        // object or in the same activity which contains the mapview.
+        Mapbox.getInstance(this, getString(R.string.access_token));
 
-    // This contains the MapView in XML and needs to be called after the account manager
-    setContentView(R.layout.activity_style_default);
+        // This contains the MapView in XML and needs to be called after the account manager
+        setContentView(R.layout.activity_style_default);
 
-    mapView = (MapView) findViewById(R.id.mapView);
-    mapView.onCreate(savedInstanceState);
-    mapView.getMapAsync(new OnMapReadyCallback() {
-      @Override
-      public void onMapReady(MapboxMap mapboxMap) {
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                map = mapboxMap;
 
-        // customize map with markers, polylines, etc
+            }
+        });
+    }
 
-      }
-    });
-  }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
 
-  @Override
-  public void onResume() {
-    super.onResume();
-    mapView.onResume();
-  }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
 
-  @Override
-  protected void onStart() {
-    super.onStart();
-    mapView.onStart();
-  }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
 
-  @Override
-  protected void onStop() {
-    super.onStop();
-    mapView.onStop();
-  }
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
 
-  @Override
-  public void onPause() {
-    super.onPause();
-    mapView.onPause();
-  }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
 
-  @Override
-  public void onSaveInstanceState(Bundle outState) {
-    super.onSaveInstanceState(outState);
-    mapView.onSaveInstanceState(outState);
-  }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
 
-  @Override
-  public void onLowMemory() {
-    super.onLowMemory();
-    mapView.onLowMemory();
-  }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
 
-  @Override
-  public void onDestroy() {
-    super.onDestroy();
-    mapView.onDestroy();
-  }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_map_style, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_streets:
+                map.setStyleUrl(getString(R.string.mapbox_style_mapbox_streets));
+                return true;
+            case R.id.menu_dark:
+                map.setStyleUrl(getString(R.string.mapbox_style_dark));
+                return true;
+            case R.id.menu_light:
+                map.setStyleUrl(getString(R.string.mapbox_style_light));
+                return true;
+            case R.id.menu_outdoors:
+                map.setStyleUrl(getString(R.string.mapbox_style_outdoors));
+                return true;
+            case R.id.menu_satellite:
+                map.setStyleUrl(getString(R.string.mapbox_style_satellite));
+                return true;
+            case R.id.menu_satellite_streets:
+                map.setStyleUrl(getString(R.string.mapbox_style_satellite_streets));
+                return true;
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+    }
+
 }
