3a5eb819eb2809e8e2536510b686da0b67aa738d
langsmith <langstonlmcs@gmail.com>
2017-02-10 10:46:57 -0500

    weird auto re-format of code. Didn't make any changes in this activity.

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/location/AnimatedLocationIconActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/location/AnimatedLocationIconActivity.java
@@ -217,47 +217,48 @@ public class AnimatedLocationIconActivity extends AppCompatActivity {
         };
 
     }
-        @Override
-        public void onRequestPermissionsResult (
-        int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
-            if (requestCode == PERMISSIONS_LOCATION) {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    enableLocation();
-                } else {
-                    // User denied location permission, user marker won't be shown.
-                    Toast.makeText(AnimatedLocationIconActivity.this,
-                            "Enable location for example to work properly", Toast.LENGTH_LONG).show();
-                }
-            }
-        }
 
-        // Custom marker view used for pulsing the background view of marker.
-        private static class PulseMarkerViewAdapter extends MapboxMap.MarkerViewAdapter<PulseMarkerView> {
-
-            private LayoutInflater inflater;
-
-            public PulseMarkerViewAdapter(@NonNull Context context) {
-                super(context);
-                this.inflater = LayoutInflater.from(context);
-            }
-
-            @Nullable
-            @Override
-            public View getView(@NonNull PulseMarkerView marker, @Nullable View convertView, @NonNull ViewGroup parent) {
-                ViewHolder viewHolder;
-                if (convertView == null) {
-                    viewHolder = new ViewHolder();
-                    convertView = inflater.inflate(R.layout.view_pulse_marker, parent, false);
-                    viewHolder.foregroundImageView = (ImageView) convertView.findViewById(R.id.foreground_imageView);
-                    viewHolder.backgroundImageView = (ImageView) convertView.findViewById(R.id.background_imageview);
-                    convertView.setTag(viewHolder);
-                }
-                return convertView;
-            }
-
-            private static class ViewHolder {
-                ImageView foregroundImageView;
-                ImageView backgroundImageView;
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSIONS_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                enableLocation();
+            } else {
+                // User denied location permission, user marker won't be shown.
+                Toast.makeText(AnimatedLocationIconActivity.this,
+                        "Enable location for example to work properly", Toast.LENGTH_LONG).show();
             }
         }
     }
+
+    // Custom marker view used for pulsing the background view of marker.
+    private static class PulseMarkerViewAdapter extends MapboxMap.MarkerViewAdapter<PulseMarkerView> {
+
+        private LayoutInflater inflater;
+
+        public PulseMarkerViewAdapter(@NonNull Context context) {
+            super(context);
+            this.inflater = LayoutInflater.from(context);
+        }
+
+        @Nullable
+        @Override
+        public View getView(@NonNull PulseMarkerView marker, @Nullable View convertView, @NonNull ViewGroup parent) {
+            ViewHolder viewHolder;
+            if (convertView == null) {
+                viewHolder = new ViewHolder();
+                convertView = inflater.inflate(R.layout.view_pulse_marker, parent, false);
+                viewHolder.foregroundImageView = (ImageView) convertView.findViewById(R.id.foreground_imageView);
+                viewHolder.backgroundImageView = (ImageView) convertView.findViewById(R.id.background_imageview);
+                convertView.setTag(viewHolder);
+            }
+            return convertView;
+        }
+
+        private static class ViewHolder {
+            ImageView foregroundImageView;
+            ImageView backgroundImageView;
+        }
+    }
+}
