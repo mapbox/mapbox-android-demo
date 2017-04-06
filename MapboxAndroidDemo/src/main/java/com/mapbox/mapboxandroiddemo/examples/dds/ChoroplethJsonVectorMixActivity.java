5535b8f2251dc3677c449f01e9af1b620134f599
Langston <langston.smith@mapbox.com>
2017-04-05 17:28:09 -0700

    Made progress on GL-JS to Android transition

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/dds/ChoroplethJsonVectorMixActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/dds/ChoroplethJsonVectorMixActivity.java
@@ -1,20 +1,36 @@
 package com.mapbox.mapboxandroiddemo.examples.dds;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
 import com.mapbox.mapboxandroiddemo.R;
 import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.style.layers.FillLayer;
+import com.mapbox.mapboxsdk.style.layers.Property;
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
+import com.mapbox.mapboxsdk.style.sources.VectorSource;
 
-public class ChoroplethJsonVectorMixActivity extends AppCompatActivity implements MapboxMap.OnMapClickListener, OnMapReadyCallback {
+import static com.mapbox.mapboxsdk.style.functions.Function.property;
+import static com.mapbox.mapboxsdk.style.functions.stops.Stop.stop;
+import static com.mapbox.mapboxsdk.style.functions.stops.Stops.categorical;
+
+public class ChoroplethJsonVectorMixActivity extends AppCompatActivity implements OnMapReadyCallback {
 
   private MapView mapView;
   private MapboxMap map;
+
+  private int maxValue = 13;
+  private String mapId = "mapbox.us_census_states_2015";
+  private String vtMatchProp = "STATEFP";
+  private String dataMatchProp = "STATE_ID";
+  private String dataStyleProp = "unemployment";
+
+//  TODO: Fix stops below
+//  private int[] stops = [["0","rgba(0,0,0,0)"]];
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +51,34 @@ public class ChoroplethJsonVectorMixActivity extends AppCompatActivity implement
   @Override
   public void onMapReady(MapboxMap mapboxMap) {
     map = mapboxMap;
-    map.setOnMapClickListener(ChoroplethJsonVectorMixActivity.this);
+
+    setUpLocalJsonData();
+
+    // Add source for state polygons hosted on Mapbox
+    VectorSource vectorSource = new VectorSource("vectorTiles", "mapbox://" + mapId);
+    map.addSource(vectorSource);
+
+//    TODO: First value is the default, used where the is no data
 
 
-  }
+//    TODO: Calculate color for each state based on the unemployment rate
 
-  @Override
-  public void onMapClick(@NonNull LatLng point) {
+    data.forEach(function(row) {
+      int green = ((row[dataStyleProp] / maxValue) * 255);
+      String color = "rgba(" + 0 + ", " + green + ", " + 0 + ", 1)";
+      stops.push([row[dataMatchProp], color]);
+    });
+
+    // Add layer from the vector tile source with data-driven style
+    map.addLayer(new FillLayer("states-join", "vectorTiles").withProperties(
+      PropertyFactory.fillColor(
+        property(vtMatchProp,
+          categorical(
+            stop(stops, PropertyFactory.lineColor(Color.parseColor("#F7455D")))))
+      ),
+      PropertyFactory.visibility(Property.VISIBLE),
+      PropertyFactory.lineWidth(3f)));
+
 
   }
 
@@ -85,6 +122,15 @@ public class ChoroplethJsonVectorMixActivity extends AppCompatActivity implement
   protected void onSaveInstanceState(Bundle outState) {
     super.onSaveInstanceState(outState);
     mapView.onSaveInstanceState(outState);
+  }
+
+  private void setUpLocalJsonData() {
+
+    // Join local JSON data with vector tile geometry
+    // USA unemployment rate in 2009
+    // Source https://data.bls.gov/timeseries/LNS14000000
+
+
   }
 
 
