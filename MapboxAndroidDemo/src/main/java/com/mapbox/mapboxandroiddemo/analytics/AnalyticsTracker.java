12ff594bb3c9702b90e779f5a51e892aa35621a3
langsmith <langstonlmcs@gmail.com>
2017-04-28 07:48:18 -0700

    removed blank lines and changed to single line methods

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/analytics/AnalyticsTracker.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/analytics/AnalyticsTracker.java
@@ -18,117 +18,105 @@ import java.util.Map;
 
 public class AnalyticsTracker {
 
-  private static final AnalyticsTracker INSTANCE = new AnalyticsTracker();
+    private static final AnalyticsTracker INSTANCE = new AnalyticsTracker();
 
-  private String openedApp = "Opened app";
-  private String clickedOnSignInButtonEventName = "Clicked on sign in button";
-  private String clickedOnCreateAccountButtonEventName = "Clicked on create account button";
-  private String clickedOnNavDrawerSectionEventName = "Clicked on nav drawer section";
-  private String clickedOnIndividualExampleEventName = "Clicked on individual example";
-  private String sectionNameMapKey = "section name";
-  private String exampleNameMapKey = "example name";
-  private String isTabletMapValue = "tablet";
-  private String isPhoneMapValue = "phone";
-
-
-  public static AnalyticsTracker get() {
-
-    return INSTANCE;
-  }
-
-  private Analytics analytics = Analytics.builder("zFLtBpautarTslr61PUbvEKXXLIoLRmq").build();
-
-  public String mapboxUsername = "LangstonSmithTestUsername";
+    private String openedApp = "Opened app";
+    private String clickedOnSignInButtonEventName = "Clicked on sign in button";
+    private String clickedOnCreateAccountButtonEventName = "Clicked on create account button";
+    private String clickedOnNavDrawerSectionEventName = "Clicked on nav drawer section";
+    private String clickedOnIndividualExampleEventName = "Clicked on individual example";
+    private String sectionNameMapKey = "section name";
+    private String exampleNameMapKey = "example name";
+    private String isTabletMapValue = "tablet";
+    private String isPhoneMapValue = "phone";
 
 
-  public void openedAppForFirstTime(@NonNull String userId, boolean isTablet) {
-
-    Map<String, String> properties = new HashMap<>();
-
-    properties.put("Device model", Build.MODEL);
-    properties.put("Device brand", Build.BRAND);
-    properties.put("Device product", Build.PRODUCT);
-    properties.put("Device manufacturer", Build.MANUFACTURER);
-    properties.put("Device device", Build.DEVICE);
-    properties.put("Device tags", Build.TAGS);
-
-    if (isTablet) {
-      properties.put("Device size", isTabletMapValue);
-    } else {
-      properties.put("Device size", isPhoneMapValue);
+    public static AnalyticsTracker get() {
+        return INSTANCE;
     }
 
+    private Analytics analytics = Analytics.builder("zFLtBpautarTslr61PUbvEKXXLIoLRmq").build();
 
-    analytics.enqueue(TrackMessage.builder("Opened App For First Time")
-      .userId(mapboxUsername)
-      .properties(properties)
-    );
+    public String mapboxUsername = "LangstonSmithTestUsername";
 
-  }
+    public void openedAppForFirstTime(@NonNull String userId, boolean isTablet) {
 
-  public void openedApp() {
-    trackEvent(openedApp, null, null);
-  }
+        Map<String, String> properties = new HashMap<>();
 
+        properties.put("Device model", Build.MODEL);
+        properties.put("Device brand", Build.BRAND);
+        properties.put("Device product", Build.PRODUCT);
+        properties.put("Device manufacturer", Build.MANUFACTURER);
+        properties.put("Device device", Build.DEVICE);
+        properties.put("Device tags", Build.TAGS);
 
-  public void clickedOnSignInButton() {
-    trackEvent(clickedOnSignInButtonEventName, null, null);
-  }
+        if (isTablet) {
+            properties.put("Device size", isTabletMapValue);
+        } else {
+            properties.put("Device size", isPhoneMapValue);
+        }
 
-  public void clickedOnCreateAccountButton() {
-    trackEvent(clickedOnCreateAccountButtonEventName, null, null);
-  }
+        analytics.enqueue(TrackMessage.builder("Opened App For First Time")
+                .userId(mapboxUsername)
+                .properties(properties)
+        );
 
-
-  public void clickedOnNavDrawerSection(@NonNull String sectionName) {
-    trackEvent(clickedOnNavDrawerSectionEventName, sectionNameMapKey, sectionName);
-
-  }
-
-  public void clickedOnIndividualExample(@NonNull String exampleName) {
-    trackEvent(clickedOnIndividualExampleEventName, exampleNameMapKey, exampleName);
-  }
-
-  public void trackEvent(@NonNull String eventName, String keyForPropertiesMap, String valueForPropertiesMap) {
-
-
-    if (keyForPropertiesMap == null || valueForPropertiesMap == null) {
-      analytics.enqueue(TrackMessage.builder(eventName)
-        .userId(mapboxUsername));
     }
 
-    if (keyForPropertiesMap != null && valueForPropertiesMap != null) {
-
-      Map<String, String> properties = new HashMap<>();
-      properties.put(keyForPropertiesMap, valueForPropertiesMap);
-
-      analytics.enqueue(TrackMessage.builder(eventName)
-        .userId(mapboxUsername)
-        .properties(properties));
+    public void openedApp() {
+        trackEvent(openedApp, null, null);
     }
 
-  }
+    public void clickedOnSignInButton() {
+        trackEvent(clickedOnSignInButtonEventName, null, null);
+    }
 
-  public void viewedScreen(String nameOfScreen) {
+    public void clickedOnCreateAccountButton() {
+        trackEvent(clickedOnCreateAccountButtonEventName, null, null);
+    }
 
-    analytics.enqueue(ScreenMessage.builder(nameOfScreen)
-      .userId(mapboxUsername)
-    );
+    public void clickedOnNavDrawerSection(@NonNull String sectionName) {
+        trackEvent(clickedOnNavDrawerSectionEventName, sectionNameMapKey, sectionName);
+    }
 
-  }
+    public void clickedOnIndividualExample(@NonNull String exampleName) {
+        trackEvent(clickedOnIndividualExampleEventName, exampleNameMapKey, exampleName);
+    }
 
-  public void identifyUser(@NonNull String actualNameOfUser, @NonNull String userEmailAddress) {
+    public void trackEvent(@NonNull String eventName, String keyForPropertiesMap, String valueForPropertiesMap) {
 
-    Map<String, String> traits = new HashMap<>();
-    traits.put("name", actualNameOfUser);
-    traits.put("email", userEmailAddress);
 
-    analytics.enqueue(IdentifyMessage.builder()
-      .userId(mapboxUsername)
-      .traits(traits)
-    );
+        if (keyForPropertiesMap == null || valueForPropertiesMap == null) {
+            analytics.enqueue(TrackMessage.builder(eventName)
+                    .userId(mapboxUsername));
+        }
 
-  }
+        if (keyForPropertiesMap != null && valueForPropertiesMap != null) {
+
+            Map<String, String> properties = new HashMap<>();
+            properties.put(keyForPropertiesMap, valueForPropertiesMap);
+
+            analytics.enqueue(TrackMessage.builder(eventName)
+                    .userId(mapboxUsername)
+                    .properties(properties));
+        }
+    }
+
+    public void viewedScreen(String nameOfScreen) {
+        analytics.enqueue(ScreenMessage.builder(nameOfScreen).userId(mapboxUsername));
+    }
+
+    public void identifyUser(@NonNull String actualNameOfUser, @NonNull String userEmailAddress) {
+
+        Map<String, String> traits = new HashMap<>();
+        traits.put("name", actualNameOfUser);
+        traits.put("email", userEmailAddress);
+
+        analytics.enqueue(IdentifyMessage.builder()
+                .userId(mapboxUsername)
+                .traits(traits)
+        );
+    }
 
 
 }
