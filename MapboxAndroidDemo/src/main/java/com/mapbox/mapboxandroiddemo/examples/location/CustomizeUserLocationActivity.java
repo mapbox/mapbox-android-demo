619389595234a5433c09497f12f99667304c482e
Langston <langston.smith@mapbox.com>
2017-05-02 17:43:05 -0400

    Adjusted activity (added LocationEngineListener, refactored gps/location method)

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/location/CustomizeUserLocationActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/location/CustomizeUserLocationActivity.java
@@ -1,6 +1,8 @@
 package com.mapbox.mapboxandroiddemo.examples.location;
 
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -8,12 +10,15 @@ import android.widget.Toast;
 
 import com.mapbox.mapboxandroiddemo.R;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.constants.MyLocationTracking;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.location.LocationSource;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.services.android.telemetry.location.LocationEngine;
+import com.mapbox.services.android.telemetry.location.LocationEngineListener;
 import com.mapbox.services.android.telemetry.permissions.PermissionsListener;
 import com.mapbox.services.android.telemetry.permissions.PermissionsManager;
 
@@ -24,6 +29,13 @@ public class CustomizeUserLocationActivity extends AppCompatActivity implements 
   private PermissionsManager permissionsManager;
   private MapView mapView;
   private MapboxMap map;
+  private LocationEngine locationEngine;
+  private LocationEngineListener locationEngineListener;
+
+
+
+  private static final int PERMISSIONS_LOCATION = 0;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +49,8 @@ public class CustomizeUserLocationActivity extends AppCompatActivity implements 
     setContentView(R.layout.activity_location_customize_user);
 
     // Get the location engine object for later use.
-    LocationEngine locationEngine = LocationSource.getLocationEngine(this);
+    locationEngine = LocationSource.getLocationEngine(this);
     locationEngine.activate();
-    locationEngine.requestLocationUpdates();
 
     mapView = (MapView) findViewById(R.id.mapView);
     mapView.onCreate(savedInstanceState);
@@ -52,13 +63,14 @@ public class CustomizeUserLocationActivity extends AppCompatActivity implements 
         if (!PermissionsManager.areLocationPermissionsGranted(CustomizeUserLocationActivity.this)) {
           permissionsManager.requestLocationPermissions(CustomizeUserLocationActivity.this);
         } else {
-          enableGps();
+          enableLocation();
         }
 
         // Customize the user location icon using the getMyLocationViewSettings object.
         map.getMyLocationViewSettings().setPadding(0, 500, 0, 0);
         map.getMyLocationViewSettings().setForegroundTintColor(Color.parseColor("#56B881"));
         map.getMyLocationViewSettings().setAccuracyTintColor(Color.parseColor("#FBB03B"));
+
 
       }
     });
@@ -113,8 +125,13 @@ public class CustomizeUserLocationActivity extends AppCompatActivity implements 
   }
 
   @Override
-  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-    permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+  public void onRequestPermissionsResult(
+    int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    if (requestCode == PERMISSIONS_LOCATION) {
+      if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        enableLocation();
+      }
+    }
   }
 
   @Override
@@ -123,10 +140,46 @@ public class CustomizeUserLocationActivity extends AppCompatActivity implements 
       Toast.LENGTH_LONG).show();
   }
 
+
+  private void enableLocation() {
+
+      // If we have the last location of the user, we can move the camera to that position.
+      Location lastLocation = locationEngine.getLastLocation();
+      if (lastLocation != null) {
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lastLocation), 16));
+      }
+
+      locationEngineListener = new LocationEngineListener() {
+        @Override
+        public void onConnected() {
+          // No action needed here.
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+          if (location != null) {
+            // Move the map camera to where the user location is and then remove the
+            // listener so the camera isn't constantly updating when the user location
+            // changes. When the user disables and then enables the location again, this
+            // listener is registered again and will adjust the camera once again.
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location), 16));
+            locationEngine.removeLocationEngineListener(this);
+          }
+        }
+      };
+
+      locationEngine.addLocationEngineListener(locationEngineListener);
+
+      // Enable or disable the location layer on the map
+    map.setMyLocationEnabled(true);
+  }
+
+
+
   @Override
   public void onPermissionResult(boolean granted) {
     if (granted) {
-      enableGps();
+      enableLocation();
     } else {
       Toast.makeText(this, getString(R.string.user_location_permission_not_granted),
         Toast.LENGTH_LONG).show();
