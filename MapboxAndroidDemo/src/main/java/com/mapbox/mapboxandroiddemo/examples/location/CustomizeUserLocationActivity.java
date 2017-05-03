00c842beb810d59959029ef304a69c2df96d27d3
Langston <langston.smith@mapbox.com>
2017-05-03 13:53:58 -0400

    Checkstyle: removed unused import

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/location/CustomizeUserLocationActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/location/CustomizeUserLocationActivity.java
@@ -1,6 +1,5 @@
 package com.mapbox.mapboxandroiddemo.examples.location;
 
-import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
