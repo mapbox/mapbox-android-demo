6681c5f170c95533e9da37030c74c43568bab3d6
Langston <langston.smith@mapbox.com>
2017-05-11 10:07:34 -0700

    Checkstyle adjustments to user pojos

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/model/usermodel/Plan.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/model/usermodel/Plan.java
@@ -5,203 +5,203 @@ package com.mapbox.mapboxandroiddemo.model.usermodel;
  */
 
 public class Plan {
-    private String satellite;
+  private String satellite;
 
-    private Services services;
+  private Services services;
 
-    private String datasetStorage;
+  private String datasetStorage;
 
-    private String support;
+  private String support;
 
-    private String geometries;
+  private String geometries;
 
-    private String rawData;
+  private String rawData;
 
-    private String restricted;
+  private String restricted;
 
-    private String styles;
+  private String styles;
 
-    private String period;
+  private String period;
 
-    private String tm2z;
+  private String tm2z;
 
-    private String id;
+  private String id;
 
-    private String watermark;
+  private String watermark;
 
-    private String price;
+  private String price;
 
-    private String hidden;
+  private String hidden;
 
-    private String views;
+  private String views;
 
-    private String name;
+  private String name;
 
-    private String privateTilesets;
+  private String privateTilesets;
 
-    private String wmts;
+  private String wmts;
 
-    private String paywall;
+  private String paywall;
 
-    private String storage;
+  private String storage;
 
-    public String getSatellite() {
-        return satellite;
-    }
+  public String getSatellite() {
+    return satellite;
+  }
 
-    public void setSatellite(String satellite) {
-        this.satellite = satellite;
-    }
+  public void setSatellite(String satellite) {
+    this.satellite = satellite;
+  }
 
-    public Services getServices() {
-        return services;
-    }
+  public Services getServices() {
+    return services;
+  }
 
-    public void setServices(Services services) {
-        this.services = services;
-    }
+  public void setServices(Services services) {
+    this.services = services;
+  }
 
-    public String getDatasetStorage() {
-        return datasetStorage;
-    }
+  public String getDatasetStorage() {
+    return datasetStorage;
+  }
 
-    public void setDatasetStorage(String datasetStorage) {
-        this.datasetStorage = datasetStorage;
-    }
+  public void setDatasetStorage(String datasetStorage) {
+    this.datasetStorage = datasetStorage;
+  }
 
-    public String getSupport() {
-        return support;
-    }
+  public String getSupport() {
+    return support;
+  }
 
-    public void setSupport(String support) {
-        this.support = support;
-    }
+  public void setSupport(String support) {
+    this.support = support;
+  }
 
-    public String getGeometries() {
-        return geometries;
-    }
+  public String getGeometries() {
+    return geometries;
+  }
 
-    public void setGeometries(String geometries) {
-        this.geometries = geometries;
-    }
+  public void setGeometries(String geometries) {
+    this.geometries = geometries;
+  }
 
-    public String getRawData() {
-        return rawData;
-    }
+  public String getRawData() {
+    return rawData;
+  }
 
-    public void setRawData(String rawData) {
-        this.rawData = rawData;
-    }
+  public void setRawData(String rawData) {
+    this.rawData = rawData;
+  }
 
-    public String getRestricted() {
-        return restricted;
-    }
+  public String getRestricted() {
+    return restricted;
+  }
 
-    public void setRestricted(String restricted) {
-        this.restricted = restricted;
-    }
+  public void setRestricted(String restricted) {
+    this.restricted = restricted;
+  }
 
-    public String getStyles() {
-        return styles;
-    }
+  public String getStyles() {
+    return styles;
+  }
 
-    public void setStyles(String styles) {
-        this.styles = styles;
-    }
+  public void setStyles(String styles) {
+    this.styles = styles;
+  }
 
-    public String getPeriod() {
-        return period;
-    }
+  public String getPeriod() {
+    return period;
+  }
 
-    public void setPeriod(String period) {
-        this.period = period;
-    }
+  public void setPeriod(String period) {
+    this.period = period;
+  }
 
-    public String getTm2z() {
-        return tm2z;
-    }
+  public String getTm2z() {
+    return tm2z;
+  }
 
-    public void setTm2z(String tm2z) {
-        this.tm2z = tm2z;
-    }
+  public void setTm2z(String tm2z) {
+    this.tm2z = tm2z;
+  }
 
-    public String getId() {
-        return id;
-    }
+  public String getId() {
+    return id;
+  }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+  public void setId(String id) {
+    this.id = id;
+  }
 
-    public String getWatermark() {
-        return watermark;
-    }
+  public String getWatermark() {
+    return watermark;
+  }
 
-    public void setWatermark(String watermark) {
-        this.watermark = watermark;
-    }
+  public void setWatermark(String watermark) {
+    this.watermark = watermark;
+  }
 
-    public String getPrice() {
-        return price;
-    }
+  public String getPrice() {
+    return price;
+  }
 
-    public void setPrice(String price) {
-        this.price = price;
-    }
+  public void setPrice(String price) {
+    this.price = price;
+  }
 
-    public String getHidden() {
-        return hidden;
-    }
+  public String getHidden() {
+    return hidden;
+  }
 
-    public void setHidden(String hidden) {
-        this.hidden = hidden;
-    }
+  public void setHidden(String hidden) {
+    this.hidden = hidden;
+  }
 
-    public String getViews() {
-        return views;
-    }
+  public String getViews() {
+    return views;
+  }
 
-    public void setViews(String views) {
-        this.views = views;
-    }
+  public void setViews(String views) {
+    this.views = views;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getPrivateTilesets() {
-        return privateTilesets;
-    }
+  public String getPrivateTilesets() {
+    return privateTilesets;
+  }
 
-    public void setPrivateTilesets(String privateTilesets) {
-        this.privateTilesets = privateTilesets;
-    }
+  public void setPrivateTilesets(String privateTilesets) {
+    this.privateTilesets = privateTilesets;
+  }
 
-    public String getWmts() {
-        return wmts;
-    }
+  public String getWmts() {
+    return wmts;
+  }
 
-    public void setWmts(String wmts) {
-        this.wmts = wmts;
-    }
+  public void setWmts(String wmts) {
+    this.wmts = wmts;
+  }
 
-    public String getPaywall() {
-        return paywall;
-    }
+  public String getPaywall() {
+    return paywall;
+  }
 
-    public void setPaywall(String paywall) {
-        this.paywall = paywall;
-    }
+  public void setPaywall(String paywall) {
+    this.paywall = paywall;
+  }
 
-    public String getStorage() {
-        return storage;
-    }
+  public String getStorage() {
+    return storage;
+  }
 
-    public void setStorage(String storage) {
-        this.storage = storage;
-    }
+  public void setStorage(String storage) {
+    this.storage = storage;
+  }
 }
--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/model/usermodel/Services.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/model/usermodel/Services.java
@@ -4,41 +4,34 @@ package com.mapbox.mapboxandroiddemo.model.usermodel;
  * Created by LangstonSmith on 5/9/17.
  */
 
-public class Services
-{
-    private UserDirections directions;
+public class Services {
+  private UserDirections directions;
 
-    private Mapviews mapviews;
+  private Mapviews mapviews;
 
-    private Geocodes geocodes;
+  private Geocodes geocodes;
 
-    public UserDirections getDirections ()
-    {
-        return directions;
-    }
+  public UserDirections getDirections() {
+    return directions;
+  }
 
-    public void setDirections(UserDirections directions)
-    {
-        this.directions = directions;
-    }
+  public void setDirections(UserDirections directions) {
+    this.directions = directions;
+  }
 
-    public Mapviews getMapviews ()
-    {
-        return mapviews;
-    }
+  public Mapviews getMapviews() {
+    return mapviews;
+  }
 
-    public void setMapviews(Mapviews mapviews)
-    {
-        this.mapviews = mapviews;
-    }
+  public void setMapviews(Mapviews mapviews) {
+    this.mapviews = mapviews;
+  }
 
-    public Geocodes getGeocodes ()
-    {
-        return geocodes;
-    }
+  public Geocodes getGeocodes() {
+    return geocodes;
+  }
 
-    public void setGeocodes(Geocodes geocodes)
-    {
-        this.geocodes = geocodes;
-    }
+  public void setGeocodes(Geocodes geocodes) {
+    this.geocodes = geocodes;
+  }
 }
