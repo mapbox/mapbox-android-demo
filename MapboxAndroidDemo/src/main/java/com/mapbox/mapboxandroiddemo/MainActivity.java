e1c66044b2fdb91a6d587f7f70f9aa5041566686
Langston <langston.smith@mapbox.com>
2017-06-07 15:05:58 -0700

    Initial commit of example (layout, example card, onMapReady, etc.)

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
@@ -37,6 +37,7 @@ import com.mapbox.mapboxandroiddemo.examples.camera.RestrictCameraActivity;
 import com.mapbox.mapboxandroiddemo.examples.dds.ChoroplethZoomChangeActivity;
 import com.mapbox.mapboxandroiddemo.examples.dds.StyleCirclesCategoricallyActivity;
 import com.mapbox.mapboxandroiddemo.examples.dds.StyleLineIdentityPropertyActivity;
+import com.mapbox.mapboxandroiddemo.examples.extrusions.MarathonExtrusionActivity;
 import com.mapbox.mapboxandroiddemo.examples.location.AnimatedLocationIconActivity;
 import com.mapbox.mapboxandroiddemo.examples.location.BasicUserLocation;
 import com.mapbox.mapboxandroiddemo.examples.location.CustomizeUserLocationActivity;
@@ -271,6 +272,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
           new Intent(MainActivity.this, PopulationDensityExtrusionActivity.class),
           R.string.activity_style_population_density_extrusions_url, true
         ));
+        exampleItemModel.add(new ExampleItemModel(
+          R.string.activity_style_catalina_marathon_extrusions_title,
+          R.string.activity_style_catalina_marathon_extrusions_description,
+          new Intent(MainActivity.this, MarathonExtrusionActivity.class),
+          R.string.activity_style_catalina_marathon_extrusions_url, true
+        ));
         break;
 
       case R.id.nav_annotations:
@@ -302,7 +309,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
           R.string.activity_annotation_polygon_holes_title,
           R.string.activity_annotation_polygon_holes_description,
           new Intent(MainActivity.this, PolygonHolesActivity.class),
-          R.string.activity_annotation_polygon_holes_url,true
+          R.string.activity_annotation_polygon_holes_url, true
         ));
         exampleItemModel.add(new ExampleItemModel(
           R.string.activity_annotation_custom_info_window_title,
