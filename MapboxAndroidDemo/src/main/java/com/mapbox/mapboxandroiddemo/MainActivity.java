5aa7aadd61dbc34a630bc356ebfe76185ace30ad
Langston Smith <langston.smith@mapbox.com>
2017-06-22 11:40:46 -0700

    Removed line

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
@@ -159,7 +159,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
       }
     });
 
-
     DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
     ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
       this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
