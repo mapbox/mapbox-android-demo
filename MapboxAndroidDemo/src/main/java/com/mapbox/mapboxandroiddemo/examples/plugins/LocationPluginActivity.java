98e422b81e1f1d1cb1f3237b939e774df4b35e62
Langston Smith <langston.smith@mapbox.com>
2017-07-19 10:51:57 -0700

    initial set up of location plugin example

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/plugins/LocationPluginActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/plugins/LocationPluginActivity.java
@@ -1,19 +1,30 @@
 package com.mapbox.mapboxandroiddemo.examples.plugins;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 import com.mapbox.mapboxandroiddemo.R;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerMode;
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
+import com.mapbox.services.android.location.LostLocationEngine;
+import com.mapbox.services.android.telemetry.location.LocationEngine;
+import com.mapbox.services.android.telemetry.location.LocationEngineListener;
+import com.mapbox.services.android.telemetry.location.LocationEnginePriority;
 
-public class LocationPluginActivity extends AppCompatActivity {
+public class LocationPluginActivity extends AppCompatActivity implements LocationEngineListener {
 
-  private MapView mapView;
+  private LocationLayerPlugin locationPlugin;
+  private LocationEngine locationEngine;
   private MapboxMap mapboxMap;
+  private MapView mapView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -32,33 +43,61 @@ public class LocationPluginActivity extends AppCompatActivity {
       @Override
       public void onMapReady(MapboxMap mapboxMap) {
         LocationPluginActivity.this.mapboxMap = mapboxMap;
+
+        locationEngine = new LostLocationEngine(LocationPluginActivity.this);
+
+        locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
+        locationEngine.activate();
+        locationPlugin = new LocationLayerPlugin(mapView, mapboxMap, locationEngine);
+        locationPlugin.setLocationLayerEnabled(LocationLayerMode.NONE);
       }
     });
 
   }
+  @Override
+  protected void onStart() {
+    super.onStart();
+    if (locationPlugin != null) {
+      locationPlugin.onStart();
+    }
+    mapView.onStart();
+  }
 
   @Override
-  public void onResume() {
+  protected void onResume() {
     super.onResume();
     mapView.onResume();
   }
 
   @Override
-  protected void onStart() {
-    super.onStart();
-    mapView.onStart();
+  protected void onPause() {
+    super.onPause();
+    mapView.onPause();
   }
 
   @Override
   protected void onStop() {
     super.onStop();
+    locationPlugin.onStop();
+    if (locationEngine != null) {
+      locationEngine.removeLocationUpdates();
+    }
     mapView.onStop();
   }
 
   @Override
-  public void onPause() {
-    super.onPause();
-    mapView.onPause();
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    mapView.onSaveInstanceState(outState);
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    mapView.onDestroy();
+    if (locationEngine != null) {
+      locationEngine.deactivate();
+    }
   }
 
   @Override
@@ -68,15 +107,15 @@ public class LocationPluginActivity extends AppCompatActivity {
   }
 
   @Override
-  protected void onDestroy() {
-    super.onDestroy();
-    mapView.onDestroy();
+  @SuppressWarnings( {"MissingPermission"})
+  public void onConnected() {
+    locationEngine.requestLocationUpdates();
   }
 
   @Override
-  protected void onSaveInstanceState(Bundle outState) {
-    super.onSaveInstanceState(outState);
-    mapView.onSaveInstanceState(outState);
+  public void onLocationChanged(Location location) {
+    mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+      new LatLng(location.getLatitude(), location.getLongitude()), 16));
   }
 
   public boolean onOptionsItemSelected(MenuItem item) {
