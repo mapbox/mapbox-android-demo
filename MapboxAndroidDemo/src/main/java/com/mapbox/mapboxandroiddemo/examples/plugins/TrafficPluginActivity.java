b606e093c0b284a16409e2c0ee47d700b223bd12
Cameron Mace <cameron@mapbox.com>
2017-07-19 15:04:46 -0400

    fixed checkstyle

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/plugins/TrafficPluginActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/plugins/TrafficPluginActivity.java
@@ -2,7 +2,6 @@ package com.mapbox.mapboxandroiddemo.examples.plugins;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.mapbox.mapboxandroiddemo.R;
