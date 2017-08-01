5550fef4e5a2cd5a0376af6e45b298f7da48067a
electrostat <agbyaj@gmail.com>
2017-08-01 18:41:53 -0400

    Code cleanup
    - remove unused variables, methods, and imports

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/extrusions/RotationExtrusionActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/extrusions/RotationExtrusionActivity.java
@@ -1,48 +1,29 @@
 package com.mapbox.mapboxandroiddemo.examples.extrusions;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.hardware.GeomagneticField;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.hardware.SensorListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.mapbox.mapboxandroiddemo.R;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
-import com.mapbox.mapboxsdk.geometry.LatLng;
-import com.mapbox.mapboxsdk.location.LocationSource;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
 import com.mapbox.mapboxsdk.style.functions.Function;
 import com.mapbox.mapboxsdk.style.functions.stops.IdentityStops;
 import com.mapbox.mapboxsdk.style.layers.FillExtrusionLayer;
-import com.mapbox.mapboxsdk.style.layers.Property;
-import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
-import com.mapbox.mapboxsdk.style.light.Light;
-import com.mapbox.mapboxsdk.style.light.Position;
-import com.mapbox.services.android.telemetry.location.LocationEngine;
 
-import java.util.List;
 
-import static com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerMode.COMPASS;
 import static com.mapbox.mapboxsdk.style.layers.Filter.eq;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillExtrusionBase;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillExtrusionColor;
@@ -53,13 +34,6 @@ public class RotationExtrusionActivity extends AppCompatActivity implements Sens
 
     private MapView mapView;
     private MapboxMap mapboxMap;
-    private Light light;
-    private boolean isMapAnchorLight;
-    private boolean isLowIntensityLight;
-    private boolean isRedColor;
-    private boolean isInitPosition;
-    private LocationEngine locationEngine;
-    private LocationLayerPlugin locationLayerPlugin;
     private SensorManager sensorManager;
     private Sensor gyro;
     private Sensor magnetic;
@@ -87,7 +61,6 @@ public class RotationExtrusionActivity extends AppCompatActivity implements Sens
             public void onMapReady(@NonNull final MapboxMap map) {
                 mapboxMap = map;
                 setupBuildings();
-                setupLocationPlugin();
             }
         });
 
@@ -115,37 +88,14 @@ public class RotationExtrusionActivity extends AppCompatActivity implements Sens
         mapboxMap.addLayer(fillExtrusionLayer);
     }
 
-    private void setupLocationPlugin(){
-//        locationEngine = new LocationSource(this);
-//        locationLayerPlugin = new LocationLayerPlugin(mapView, mapboxMap, locationEngine);
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            return;
-//        }
-//        locationLayerPlugin.setLocationLayerEnabled(COMPASS);
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_building, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (light != null) {
-            int id = item.getItemId();
-            if (id == R.id.menu_action_anchor) {
-                isMapAnchorLight = !isMapAnchorLight;
-                light.setAnchor(isMapAnchorLight ? Property.ANCHOR_MAP : Property.ANCHOR_VIEWPORT);
-            } else if (id == R.id.menu_action_intensity) {
-                isLowIntensityLight = !isLowIntensityLight;
-                light.setIntensity(isLowIntensityLight ? 0.35f : 1.0f);
-            } else if (id == android.R.id.home) {
-                finish();
-                return true;
-            }
-        }
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 
     @Override
