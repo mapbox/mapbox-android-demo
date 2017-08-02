23cc9f0ad7abed4d0005bba5186472e9eeacb79b
electrostat <agbyaj@gmail.com>
2017-08-02 10:20:56 -0400

    Checkstyle
    - added and made changes to match MapBox checkstyle

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/extrusions/RotationExtrusionActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/extrusions/RotationExtrusionActivity.java
@@ -32,152 +32,155 @@ import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillExtrusionOpa
 /**
  * Change the bearing and tilt of camera when viewing extrusions to get full 3D effect
  */
-public class RotationExtrusionActivity extends AppCompatActivity implements SensorEventListener{
+public class RotationExtrusionActivity extends AppCompatActivity implements SensorEventListener {
+  private MapView mapView;
+  private MapboxMap mapboxMap;
+  private SensorManager sensorManager;
+  private Sensor gyro;
+  private Sensor magnetic;
+  float[] gravArray;
+  float[] magneticArray;
+  float azimut;
+  float pitch;
+  float roll;
 
-    private MapView mapView;
-    private MapboxMap mapboxMap;
-    private SensorManager sensorManager;
-    private Sensor gyro;
-    private Sensor magnetic;
-    float[] gravArray;
-    float[] magneticArray;
-    float azimut;
-    float pitch;
-    float roll;
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    // Mapbox access token is configured here. This needs to be called either in your application
+    // object or in the same activity which contains the mapview.
+    Mapbox.getInstance(this, getString(R.string.access_token));
 
-        // Mapbox access token is configured here. This needs to be called either in your application
-        // object or in the same activity which contains the mapview.
-        Mapbox.getInstance(this, getString(R.string.access_token));
+    // This contains the MapView in XML and needs to be called after the access token is configured.
+    setContentView(R.layout.activity_extrusion_rotation);
 
-        // This contains the MapView in XML and needs to be called after the access token is configured.
-        setContentView(R.layout.activity_extrusion_rotation);
-
-        mapView = (MapView) findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(@NonNull final MapboxMap map) {
-                mapboxMap = map;
-                setupBuildings();
-            }
-        });
-
-        //initialize sensors
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-
-        gyro = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        magnetic = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-
-        sensorManager.registerListener(this, gyro , 200);
-        sensorManager.registerListener(this, magnetic , 200);
-    }
-
-    private void setupBuildings() {
-        FillExtrusionLayer fillExtrusionLayer = new FillExtrusionLayer("3d-buildings", "composite");
-        fillExtrusionLayer.setSourceLayer("building");
-        fillExtrusionLayer.setFilter(eq("extrude", "true"));
-        fillExtrusionLayer.setMinZoom(15);
-        fillExtrusionLayer.setProperties(
-                fillExtrusionColor(Color.LTGRAY),
-                fillExtrusionHeight(Function.property("height", new IdentityStops<Float>())),
-                fillExtrusionBase(Function.property("min_height", new IdentityStops<Float>())),
-                fillExtrusionOpacity(0.9f)
-        );
-        mapboxMap.addLayer(fillExtrusionLayer);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return true;
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mapView.onStart();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mapView.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mapView.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mapView.onStop();
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
-            gravArray = event.values;
-        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
-            magneticArray = event.values;
-        if (gravArray != null && magneticArray != null) {
-            float R[] = new float[9];
-            float I[] = new float[9];
-            boolean success = SensorManager.getRotationMatrix(R, I, gravArray, magneticArray);
-            if (success) {
-                float orientation[] = new float[3];
-                SensorManager.getOrientation(R, orientation);
-                azimut = orientation[0];
-                pitch = orientation[1];
-                roll = orientation[2];
-
-                CameraPosition position = new CameraPosition.Builder()
-                        .tilt(pitch * -90)
-                        .bearing(roll * 90)
-                        .build();
-
-                if(mapboxMap != null){
-                    mapboxMap.animateCamera(CameraUpdateFactory
-                            .newCameraPosition(position), 100
-                    );
-                }
-            }
+    mapView = (MapView) findViewById(R.id.mapView);
+    mapView.onCreate(savedInstanceState);
+    mapView.getMapAsync(new OnMapReadyCallback() {
+        @Override
+        public void onMapReady(@NonNull final MapboxMap map) {
+          mapboxMap = map;
+          setupBuildings();
         }
+    });
 
+    //initialize sensors
+    sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+    gyro = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    magnetic = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+    sensorManager.registerListener(this, gyro , 200);
+    sensorManager.registerListener(this, magnetic , 200);
+  }
+
+  private void setupBuildings() {
+    FillExtrusionLayer fillExtrusionLayer = new FillExtrusionLayer("3d-buildings", "composite");
+    fillExtrusionLayer.setSourceLayer("building");
+    fillExtrusionLayer.setFilter(eq("extrude", "true"));
+    fillExtrusionLayer.setMinZoom(15);
+    fillExtrusionLayer.setProperties(
+            fillExtrusionColor(Color.LTGRAY),
+            fillExtrusionHeight(Function.property("height", new IdentityStops<Float>())),
+            fillExtrusionBase(Function.property("min_height", new IdentityStops<Float>())),
+            fillExtrusionOpacity(0.9f)
+    );
+    mapboxMap.addLayer(fillExtrusionLayer);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    return true;
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    mapView.onStart();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    mapView.onResume();
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    mapView.onPause();
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    mapView.onStop();
+  }
+
+  @Override
+  public void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    mapView.onSaveInstanceState(outState);
+  }
+
+  @Override
+  public void onLowMemory() {
+    super.onLowMemory();
+    mapView.onLowMemory();
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    mapView.onDestroy();
+  }
+
+  @Override
+  public void onSensorChanged(SensorEvent event) {
+
+    if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+      gravArray = event.values;
     }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
+    if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+      magneticArray = event.values;
     }
+
+    if (gravArray != null && magneticArray != null) {
+      float[] num1 = new float[9];
+      float[] num2 = new float[9];
+      boolean success = SensorManager.getRotationMatrix(num1, num2, gravArray, magneticArray);
+      if (success) {
+        float[] orientation = new float[3];
+        SensorManager.getOrientation(num1, orientation);
+        azimut = orientation[0];
+        pitch = orientation[1];
+        roll = orientation[2];
+
+        CameraPosition position = new CameraPosition.Builder()
+                .tilt(pitch * -90)
+                .bearing(roll * 90)
+                .build();
+
+        if (mapboxMap != null) {
+          mapboxMap.animateCamera(CameraUpdateFactory
+                  .newCameraPosition(position), 100
+          );
+        }
+      }
+    }
+
+  }
+
+  @Override
+  public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+  }
 }
