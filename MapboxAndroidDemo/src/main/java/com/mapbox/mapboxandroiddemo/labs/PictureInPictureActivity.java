f18b97cb5ea0200730a7c1fcabc500ae87b094fe
Langston Smith <langston.smith@mapbox.com>
2017-08-02 13:17:38 -0700

    checkstyle

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/labs/PictureInPictureActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/labs/PictureInPictureActivity.java
@@ -3,7 +3,6 @@ package com.mapbox.mapboxandroiddemo.labs;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
