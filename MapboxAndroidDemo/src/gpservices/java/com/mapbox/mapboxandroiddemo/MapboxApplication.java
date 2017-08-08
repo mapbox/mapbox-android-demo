601dcdcb8971f48b431dcaa234eb3aa63a436bcc
Langston Smith <langston.smith@mapbox.com>
2017-08-08 13:16:09 -0700

    Added picasso builder code to MapboxApplication

--- MapboxAndroidDemo/src/gpservices/java/com/mapbox/mapboxandroiddemo/MapboxApplication.java
+++ MapboxAndroidDemo/src/gpservices/java/com/mapbox/mapboxandroiddemo/MapboxApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.squareup.picasso.OkHttpDownloader;
+import com.squareup.picasso.Picasso;
 
 public class MapboxApplication extends Application {
 
@@ -15,5 +17,11 @@ public class MapboxApplication extends Application {
       .setApplicationId(getString(R.string.firebase_app_id))
       .build()
     );
+
+    Picasso.Builder builder = new Picasso.Builder(this);
+    builder.downloader(new OkHttpDownloader(this, Integer.MAX_VALUE));
+    Picasso built = builder.build();
+    built.setLoggingEnabled(true);
+    Picasso.setSingletonInstance(built);
   }
 }
