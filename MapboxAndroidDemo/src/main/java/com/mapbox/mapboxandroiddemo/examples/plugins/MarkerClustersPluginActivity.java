01d86f8f6b97422e5aeae94fedf4af2f59a486f3
Langston Smith <langston.smith@mapbox.com>
2017-10-30 15:46:04 -0700

    more additions

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/plugins/MarkerClustersPluginActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/plugins/MarkerClustersPluginActivity.java
@@ -2,16 +2,30 @@ package com.mapbox.mapboxandroiddemo.examples.plugins;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.mapbox.mapboxandroiddemo.R;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
-public class MarkerClustersPluginActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MarkerClustersPluginActivity extends AppCompatActivity implements OnMapReadyCallback {
 
   private MapView mapView;
+  private MapboxMap mapboxMap;
+  private ClusterManagerPlugin<MyItem> clusterManagerPlugin;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +40,13 @@ public class MarkerClustersPluginActivity extends AppCompatActivity {
 
     mapView = (MapView) findViewById(R.id.mapView);
     mapView.onCreate(savedInstanceState);
-    mapView.getMapAsync(new OnMapReadyCallback() {
-      @Override
-      public void onMapReady(MapboxMap mapboxMap) {
+    mapView.getMapAsync(this);
+  }
 
-
-
-
-      }
-    });
+  @Override
+  public void onMapReady(MapboxMap mapboxMap) {
+    this.mapboxMap = mapboxMap;
+    startDemo();
   }
 
   // Add the mapView lifecycle to the activity's lifecycle methods
@@ -78,5 +90,92 @@ public class MarkerClustersPluginActivity extends AppCompatActivity {
   protected void onSaveInstanceState(Bundle outState) {
     super.onSaveInstanceState(outState);
     mapView.onSaveInstanceState(outState);
+  }
+
+
+  protected void startDemo() {
+    mapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.503186, -0.126446), 10));
+
+    clusterManagerPlugin = new ClusterManagerPlugin<>(this, mapboxMap);
+    mapboxMap.addOnCameraIdleListener(clusterManagerPlugin);
+
+    try {
+      readItems();
+    } catch (JSONException exception) {
+      Toast.makeText(this, "Problem reading list of markers.", Toast.LENGTH_LONG).show();
+    }
+  }
+
+  private void readItems() throws JSONException {
+    InputStream inputStream = getResources().openRawResource(R.raw.radar_search);
+    List<MyItem> items = new MyItemReader().read(inputStream);
+    clusterManagerPlugin.addItems(items);
+  }
+
+  public static class MyItem implements ClusterItem {
+    private final LatLng mPosition;
+    private String mTitle;
+    private String mSnippet;
+
+    public MyItem(double lat, double lng) {
+      mPosition = new LatLng(lat, lng);
+      mTitle = null;
+      mSnippet = null;
+    }
+
+    public MyItem(double lat, double lng, String title, String snippet) {
+      mPosition = new LatLng(lat, lng);
+      mTitle = title;
+      mSnippet = snippet;
+    }
+
+    @Override
+    public LatLng getPosition() {
+      return mPosition;
+    }
+
+    @Override
+    public String getTitle() {
+      return mTitle;
+    }
+
+    @Override
+    public String getSnippet() {
+      return mSnippet;
+    }
+
+    public void setTitle(String title) {
+      mTitle = title;
+    }
+
+    public void setSnippet(String snippet) {
+      mSnippet = snippet;
+    }
+  }
+
+  public static class MyItemReader {
+
+    private static final String REGEX_INPUT_BOUNDARY_BEGINNING = "\\A";
+
+    public List<MyItem> read(InputStream inputStream) throws JSONException {
+      List<MyItem> items = new ArrayList<MyItem>();
+      String json = new Scanner(inputStream).useDelimiter(REGEX_INPUT_BOUNDARY_BEGINNING).next();
+      JSONArray array = new JSONArray(json);
+      for (int i = 0; i < array.length(); i++) {
+        String title = null;
+        String snippet = null;
+        JSONObject object = array.getJSONObject(i);
+        double lat = object.getDouble("lat");
+        double lng = object.getDouble("lng");
+        if (!object.isNull("title")) {
+          title = object.getString("title");
+        }
+        if (!object.isNull("snippet")) {
+          snippet = object.getString("snippet");
+        }
+        items.add(new MyItem(lat, lng, title, snippet));
+      }
+      return items;
+    }
   }
 }
