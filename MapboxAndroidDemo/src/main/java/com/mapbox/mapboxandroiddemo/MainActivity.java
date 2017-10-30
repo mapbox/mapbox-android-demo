d8beb0cd81c0145fc0bbf89ab279ed28fd51a143
Langston Smith <langston.smith@mapbox.com>
2017-10-30 11:25:01 -0700

    initial setup of example

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
@@ -67,6 +67,7 @@ import com.mapbox.mapboxandroiddemo.examples.offline.SimpleOfflineMapActivity;
 import com.mapbox.mapboxandroiddemo.examples.plugins.BuildingPluginActivity;
 import com.mapbox.mapboxandroiddemo.examples.plugins.GeoJsonPluginActivity;
 import com.mapbox.mapboxandroiddemo.examples.plugins.LocationPluginActivity;
+import com.mapbox.mapboxandroiddemo.examples.plugins.MarkerClustersPluginActivity;
 import com.mapbox.mapboxandroiddemo.examples.plugins.TrafficPluginActivity;
 import com.mapbox.mapboxandroiddemo.examples.query.ClickOnLayerActivity;
 import com.mapbox.mapboxandroiddemo.examples.query.FeatureCountActivity;
@@ -389,6 +390,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
           R.string.activity_plugins_geojson_plugin_description,
           new Intent(MainActivity.this, GeoJsonPluginActivity.class),
           R.string.activity_plugins_geojson_plugin_url, true
+        ));
+        exampleItemModel.add(new ExampleItemModel(
+          R.string.activity_plugins_marker_clusters_plugin_title,
+          R.string.activity_plugins_geojson_marker_clusters_description,
+          new Intent(MainActivity.this, MarkerClustersPluginActivity.class),
+          R.string.activity_plugins_markers_clusters_plugin_url, true
         ));
         currentCategory = R.id.nav_plugins;
         break;
