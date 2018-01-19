9844190c7a0e1012c0e5e3c1631d4ecaa5ad576a
Langston Smith <langston.smith@mapbox.com>
2018-01-19 11:19:42 -0800

    checkstyle

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/snapshot/SnapshotNotificationActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/snapshot/SnapshotNotificationActivity.java
@@ -3,7 +3,6 @@ package com.mapbox.mapboxandroiddemo.examples.snapshot;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
