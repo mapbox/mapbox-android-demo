c28e6a4f680e14822baf792cc54ae76a3a253bbf
Langston Smith <langston.smith@mapbox.com>
2018-03-07 17:17:27 -0800

    checkstyle: removed unused import line

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/plugins/LocalizationPluginActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/plugins/LocalizationPluginActivity.java
@@ -3,7 +3,6 @@ package com.mapbox.mapboxandroiddemo.examples.plugins;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 
 import com.mapbox.mapboxandroiddemo.R;
