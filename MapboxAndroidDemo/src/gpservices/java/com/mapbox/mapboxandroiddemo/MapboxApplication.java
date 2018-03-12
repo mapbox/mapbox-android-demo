7c822d4e6a23043f3c3f30306b3e9997ac95e807
Langston Smith <langston.smith@mapbox.com>
2018-03-12 14:56:05 -0700

    initial setup of dependencies

--- MapboxAndroidDemo/src/gpservices/java/com/mapbox/mapboxandroiddemo/MapboxApplication.java
+++ MapboxAndroidDemo/src/gpservices/java/com/mapbox/mapboxandroiddemo/MapboxApplication.java
@@ -1,12 +1,17 @@
 package com.mapbox.mapboxandroiddemo;
 
 import android.app.Application;
+import android.text.TextUtils;
+import android.util.Log;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.squareup.picasso.OkHttpDownloader;
 import com.squareup.picasso.Picasso;
+
+import timber.log.Timber;
 
 public class MapboxApplication extends Application {
 
@@ -14,8 +19,31 @@ public class MapboxApplication extends Application {
   public void onCreate() {
     super.onCreate();
     initializeFirebaseApp();
+    if (BuildConfig.DEBUG) {
+      Timber.plant(new Timber.DebugTree());
+    } else {
+      Timber.plant(new CrashReportingTree());
+    }
     setUpPicasso();
     Mapbox.getInstance(this, getString(R.string.access_token));
+  }
+
+  /**
+   * A tree which logs important information for crash reporting.
+   */
+  private static class CrashReportingTree extends Timber.Tree {
+    @Override
+    protected void log(int priority, String tag, String message, Throwable throwable) {
+      if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+        return;
+      }
+      if (!TextUtils.isEmpty(message)) {
+        Crashlytics.log(priority, tag, message);
+      }
+      if (throwable != null && priority == Log.ERROR || priority == Log.WARN) {
+        Crashlytics.logException(throwable);
+      }
+    }
   }
 
   private void initializeFirebaseApp() {
