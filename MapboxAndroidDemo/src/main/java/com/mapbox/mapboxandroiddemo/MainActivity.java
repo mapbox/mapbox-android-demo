c56c28ccb8101c6174c7243efcc02dca97d0adaf
Langston Smith <langston.smith@mapbox.com>
2018-04-05 13:28:49 -0700

    commented out geojson and traffic plugins

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
@@ -387,11 +387,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         break;
 
       case R.id.nav_plugins:
-        exampleItemModels.add(new ExampleItemModel(
+        /*exampleItemModels.add(new ExampleItemModel(
           R.string.activity_plugins_traffic_plugin_title,
           R.string.activity_plugins_traffic_plugin_description,
           new Intent(MainActivity.this, TrafficPluginActivity.class),
-          R.string.activity_plugins_traffic_plugin_url, false, BuildConfig.MIN_SDK_VERSION));
+          R.string.activity_plugins_traffic_plugin_url, false, BuildConfig.MIN_SDK_VERSION));*/
 
         exampleItemModels.add(new ExampleItemModel(
           R.string.activity_plugins_building_plugin_title,
@@ -405,10 +405,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
           new Intent(MainActivity.this, LocationPluginActivity.class),
           R.string.activity_plugins_location_plugin_url, false, BuildConfig.MIN_SDK_VERSION));
 
-        exampleItemModels.add(new ExampleItemModel(
+       /* exampleItemModels.add(new ExampleItemModel(
           R.string.activity_plugins_geojson_plugin_title, R.string.activity_plugins_geojson_plugin_description,
           new Intent(MainActivity.this, GeoJsonPluginActivity.class),
-          R.string.activity_plugins_geojson_plugin_url, false, BuildConfig.MIN_SDK_VERSION));
+          R.string.activity_plugins_geojson_plugin_url, false, BuildConfig.MIN_SDK_VERSION));*/
 
         exampleItemModels.add(new ExampleItemModel(
           R.string.activity_plugins_places_plugin_title, R.string.activity_plugins_places_plugin_description,
