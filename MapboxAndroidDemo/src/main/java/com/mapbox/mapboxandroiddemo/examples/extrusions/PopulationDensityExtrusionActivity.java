eda9e0cc8350f8aa342ec2a791900417fd0489e3
Langston Smith <langston.smith@mapbox.com>
2018-05-04 17:06:28 -0700

    Bumped android plugin for gradle to 3.1.0 (#694)
    * changed docker image, bumped gradle plugin version, bumped compile and buildTools
    
    bumped compileSdkVersion and buildToolsVersion
    
    adding license approve line to config file
    
    dropped buildTools backto 27.0.2 and removed license approval from config file
    
    changed docker image
    
    bumped buildToolsVersion to 27.0.3
    
    update ci image
    
    * removed empty line
    
    * added log
    
    * added ./gradlew clean in an attempt to solve access token issue
    
    * switched gradle clean order
    
    * Commented out dev config secret export
    
    * removed secret export from config file for good. not needed
    
    * removed other dev config export

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/extrusions/PopulationDensityExtrusionActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/extrusions/PopulationDensityExtrusionActivity.java
@@ -48,11 +48,12 @@ public class PopulationDensityExtrusionActivity extends AppCompatActivity implem
     // Mapbox access token is configured here. This needs to be called either in your application
     // object or in the same activity which contains the mapview.
     Mapbox.getInstance(this, getString(R.string.access_token));
+    Log.d("PopulationDensityExtrusion", "access_token = "
+      + getString(R.string.access_token));
     setContentView(R.layout.activity_population_density_extrusion);
     mapView = findViewById(R.id.mapView);
     mapView.onCreate(savedInstanceState);
     mapView.getMapAsync(this);
-
   }
 
   @Override
