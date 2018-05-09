9e9980430a86b7c9629cdeb01fc8167f4781f597
Langston Smith <langston.smith@mapbox.com>
2018-05-09 12:20:12 -0700

    Streets to Satellite fade example (#707)
    * initial example setup
    
    initial example setup
    
    refactoring
    
    * refactoring
    
    * Changes made based on Cam's review

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/MainActivity.java
@@ -90,6 +90,7 @@ import com.mapbox.mapboxandroiddemo.examples.styles.LineLayerActivity;
 import com.mapbox.mapboxandroiddemo.examples.styles.LocalStyleSourceActivity;
 import com.mapbox.mapboxandroiddemo.examples.styles.MapboxStudioStyleActivity;
 import com.mapbox.mapboxandroiddemo.examples.styles.ShowHideLayersActivity;
+import com.mapbox.mapboxandroiddemo.examples.styles.StyleFadeSwitchActivity;
 import com.mapbox.mapboxandroiddemo.examples.styles.VectorSourceActivity;
 import com.mapbox.mapboxandroiddemo.examples.styles.ZoomDependentFillColorActivity;
 import com.mapbox.mapboxandroiddemo.labs.AnimatedImageGifActivity;
@@ -364,6 +365,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
           new Intent(MainActivity.this, HillShadeActivity.class),
           R.string.activity_style_hillshade_url, false, BuildConfig.MIN_SDK_VERSION
         ));
+        exampleItemModels.add(new ExampleItemModel(
+          R.string.activity_styles_fade_switch_title,
+          R.string.activity_styles_fade_switch_description,
+          new Intent(MainActivity.this, StyleFadeSwitchActivity.class),
+          R.string.activity_styles_fade_switch_url, true, BuildConfig.MIN_SDK_VERSION
+        ));
         currentCategory = R.id.nav_styles;
         break;
       case R.id.nav_extrusions:
@@ -403,10 +410,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
       case R.id.nav_plugins:
 
         exampleItemModels.add(new ExampleItemModel(
-            R.string.activity_plugins_traffic_plugin_title,
-            R.string.activity_plugins_traffic_plugin_description,
-            new Intent(MainActivity.this, TrafficPluginActivity.class),
-            R.string.activity_plugins_traffic_plugin_url, false, BuildConfig.MIN_SDK_VERSION));
+          R.string.activity_plugins_traffic_plugin_title,
+          R.string.activity_plugins_traffic_plugin_description,
+          new Intent(MainActivity.this, TrafficPluginActivity.class),
+          R.string.activity_plugins_traffic_plugin_url, false, BuildConfig.MIN_SDK_VERSION));
 
         exampleItemModels.add(new ExampleItemModel(
           R.string.activity_plugins_building_plugin_title,
@@ -668,7 +675,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
           R.string.activity_dds_heatmap_title,
           R.string.activity_dds_heatmap_description,
           new Intent(MainActivity.this, HeatmapActivity.class),
-          R.string.activity_dds_heatmap_url, true, BuildConfig.MIN_SDK_VERSION));
+          R.string.activity_dds_heatmap_url, false, BuildConfig.MIN_SDK_VERSION));
 
         exampleItemModels.add(new ExampleItemModel(
           R.string.activity_dds_style_circle_categorically_title,
