0dd35387b088b90a11757fb9044db50f616bdf03
Langston Smith <langston.smith@mapbox.com>
2018-07-09 09:14:28 -0700

    removed log import line

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/plugins/PlaceSelectionPluginActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/plugins/PlaceSelectionPluginActivity.java
@@ -3,7 +3,6 @@ package com.mapbox.mapboxandroiddemo.examples.plugins;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
