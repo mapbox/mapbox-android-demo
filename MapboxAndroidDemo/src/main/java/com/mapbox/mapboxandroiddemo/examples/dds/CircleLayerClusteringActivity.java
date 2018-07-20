44fb18fd31998afc51ed6a0187b83e639cb15529
langsmith <langstonlmcs@gmail.com>
2018-07-20 11:31:56 -0700

    fix

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/dds/CircleLayerClusteringActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/dds/CircleLayerClusteringActivity.java
@@ -49,7 +49,7 @@ import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.textIgnorePlacem
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.textSize;
 
 /**
- * Use GeoJSON and circle layers to visualize point data as a circle clusters.
+ * Use GeoJSON and circle layers to visualize point data as circle clusters.
  */
 public class CircleLayerClusteringActivity extends AppCompatActivity {
 
