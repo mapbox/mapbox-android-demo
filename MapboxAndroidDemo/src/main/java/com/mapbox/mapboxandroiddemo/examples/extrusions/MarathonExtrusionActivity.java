d1e41fdadb5a9149ab0f6efd368f0790dc5827d8
Łukasz Paczos <lukas.paczos@gmail.com>
2018-08-14 18:08:53 +0200

    add lifecycle method to map used in the MarathonExtrusionActivity

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/extrusions/MarathonExtrusionActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/extrusions/MarathonExtrusionActivity.java
@@ -76,5 +76,47 @@ public class MarathonExtrusionActivity extends AppCompatActivity {
       fillExtrusionHeight(get("e")));
     mapboxMap.addLayer(courseExtrusionLayer);
   }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    mapView.onStart();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    mapView.onResume();
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    mapView.onPause();
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    mapView.onStop();
+  }
+
+  @Override
+  public void onLowMemory() {
+    super.onLowMemory();
+    mapView.onLowMemory();
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    mapView.onDestroy();
+  }
+
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    mapView.onSaveInstanceState(outState);
+  }
 }
 // #-end-code-snippet: marathon-extrusion-activity full-java
