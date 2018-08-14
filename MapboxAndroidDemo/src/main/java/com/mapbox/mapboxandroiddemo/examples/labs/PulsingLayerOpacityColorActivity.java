9a20ded8cc0f9f142a71bc40cd6e76dfc77d4836
Łukasz Paczos <lukas.paczos@gmail.com>
2018-08-14 18:33:59 +0200

    set content for PulsingLayerOpacityActivity

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/labs/PulsingLayerOpacityColorActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/labs/PulsingLayerOpacityColorActivity.java
@@ -51,7 +51,7 @@ public class PulsingLayerOpacityColorActivity extends AppCompatActivity implemen
     Mapbox.getInstance(this, getString(R.string.access_token));
 
     // This contains the MapView in XML and needs to be called after the access token is configured.
-
+    setContentView(R.layout.activity_lab_pulsing_layer_opacity_color);
     mapView = findViewById(R.id.mapView);
     mapView.onCreate(savedInstanceState);
     mapView.getMapAsync(this);
