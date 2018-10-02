0e06a1ad90206d36631f104f2ef86377df3a9ca1
Josh Erb <josh.erb@mapbox.com>
2018-10-02 10:11:49 -0400

    first six activities

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/annotations/AnimatedMarkerActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/annotations/AnimatedMarkerActivity.java
@@ -1,7 +1,5 @@
 package com.mapbox.mapboxandroiddemo.examples.annotations;
 
-// #-code-snippet: animated-marker-activity full-java
-
 import android.animation.ObjectAnimator;
 import android.animation.TypeEvaluator;
 import android.animation.ValueAnimator;
@@ -131,4 +129,3 @@ public class AnimatedMarkerActivity extends AppCompatActivity implements OnMapRe
     }
   }
 }
-// #-end-code-snippet: animated-marker-activity full-java
--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/annotations/DrawCustomMarkerActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/annotations/DrawCustomMarkerActivity.java
@@ -1,7 +1,5 @@
 package com.mapbox.mapboxandroiddemo.examples.annotations;
 
-// #-code-snippet: draw-custom-marker-activity full-java
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -94,4 +92,3 @@ public class DrawCustomMarkerActivity extends AppCompatActivity {
     mapView.onDestroy();
   }
 }
-// #-end-code-snippet: draw-custom-marker-activity full-java
