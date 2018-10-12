b4deda44435a3f1f0fed21ccb8fc690d38daf3c8
David Wicks <david.wicks@mapbox.com>
2018-10-12 13:08:38 -0400

    Create new empty activity for MagicWindow sample

---
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/labs/MagicWindowActivity.kt
@@ -0,0 +1,12 @@
+package com.mapbox.mapboxandroiddemo.examples.labs
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MagicWindowActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_magic_window)
+    }
+}
