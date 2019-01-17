e73d2102e0421ff464f54d5067d21674a1239487
langsmith <langstonlmcs@gmail.com>
2019-01-17 14:17:28 -0800

    removed duplicate import line

--- MapboxAndroidDemo/src/china/java/com/mapbox/mapboxandroiddemo/examples/SimpleChinaMapViewActivity.java
+++ MapboxAndroidDemo/src/china/java/com/mapbox/mapboxandroiddemo/examples/SimpleChinaMapViewActivity.java
@@ -12,7 +12,6 @@ import com.mapbox.mapboxsdk.plugins.china.constants.ChinaStyle;
 import com.mapbox.mapboxsdk.plugins.china.maps.ChinaMapView;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
-import com.mapbox.mapboxsdk.plugins.china.constants.ChinaStyle;
 
 /**
  * The most basic example of adding a government approved and performant China map to an activity.
