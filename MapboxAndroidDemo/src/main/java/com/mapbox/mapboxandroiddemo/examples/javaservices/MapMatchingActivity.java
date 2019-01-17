c4152bc96324b55095cc6c9911c4bb9e5e30ee7a
tobrun <tobrun.van.nuland@gmail.com>
2019-01-17 11:24:02 +0100

    Update map matching activity to v7.0.0

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/javaservices/MapMatchingActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/javaservices/MapMatchingActivity.java
@@ -4,10 +4,12 @@ import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.mapbox.api.matching.v5.MapboxMapMatching;
+import com.mapbox.api.matching.v5.models.MapMatchingMatching;
 import com.mapbox.api.matching.v5.models.MapMatchingResponse;
 import com.mapbox.core.exceptions.ServicesException;
 import com.mapbox.geojson.Feature;
@@ -22,13 +24,13 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
 import com.mapbox.mapboxsdk.style.layers.LineLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.mapboxsdk.utils.ColorUtils;
 
-import java.io.BufferedReader;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
+import java.lang.ref.WeakReference;
 import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -71,7 +73,7 @@ public class MapMatchingActivity extends AppCompatActivity {
         map.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
           @Override
           public void onStyleLoaded(@NonNull Style style) {
-            new DrawGeoJson().execute();
+            new LoadGeoJson(MapMatchingActivity.this).execute();
           }
         });
       }
@@ -120,99 +122,111 @@ public class MapMatchingActivity extends AppCompatActivity {
     mapView.onSaveInstanceState(outState);
   }
 
-  private class DrawGeoJson extends AsyncTask<Void, Void, List<Point>> {
+  private static class LoadGeoJson extends AsyncTask<Void, Void, FeatureCollection> {
+
+    private WeakReference<MapMatchingActivity> weakReference;
+
+    LoadGeoJson(MapMatchingActivity activity) {
+      this.weakReference = new WeakReference<>(activity);
+    }
+
     @Override
-    protected List<Point> doInBackground(Void... voids) {
-
-      List<Point> points = new ArrayList<>();
-
+    protected FeatureCollection doInBackground(Void... voids) {
       try {
-        // Load GeoJSON file
-        InputStream inputStream = getAssets().open("trace.geojson");
-        BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
-        StringBuilder sb = new StringBuilder();
-        int cp;
-        while ((cp = rd.read()) != -1) {
-          sb.append((char) cp);
-        }
-        inputStream.close();
-        FeatureCollection featureCollection = FeatureCollection.fromJson(sb.toString());
-        LineString lineString = (LineString) featureCollection.features().get(0).geometry();
-        for (Point singlePosition : lineString.coordinates()) {
-          points.add(Point.fromLngLat(singlePosition.longitude(),
-            singlePosition.latitude()));
+        MapMatchingActivity activity = weakReference.get();
+        if (activity != null) {
+          InputStream inputStream = activity.getAssets().open("trace.geojson");
+          return FeatureCollection.fromJson(convertStreamToString(inputStream));
         }
       } catch (Exception exception) {
         Log.e(TAG, "Exception Loading GeoJSON: " + exception.toString());
       }
-      return points;
+      return null;
+    }
+
+    static String convertStreamToString(InputStream is) {
+      Scanner scanner = new Scanner(is).useDelimiter("\\A");
+      return scanner.hasNext() ? scanner.next() : "";
     }
 
     @Override
-    protected void onPostExecute(List<Point> points) {
-      super.onPostExecute(points);
-      drawBeforeMapMatching(points);
-      drawMapMatched(points);
+    protected void onPostExecute(@Nullable FeatureCollection featureCollection) {
+      super.onPostExecute(featureCollection);
+      MapMatchingActivity activity = weakReference.get();
+      if (activity != null && featureCollection != null) {
+        activity.drawLines(featureCollection);
+      }
     }
   }
 
-  private void drawBeforeMapMatching(List<Point> points) {
-    List<Feature> featureList = new ArrayList<>();
-    for (int i = 0; i < points.size(); i++) {
-      featureList.add(Feature.fromGeometry(Point.fromLngLat(points.get(i).longitude(), points.get(i).latitude())));
+  private void drawLines(@NonNull FeatureCollection featureCollection) {
+    List<Feature> features = featureCollection.features();
+    if (features != null && features.size() > 0) {
+      Feature feature = features.get(0);
+      drawBeforeMapMatching(feature);
+      requestMapMatched(feature);
     }
-
-    GeoJsonSource geoJsonSource = new GeoJsonSource("pre-matched-source-id",
-      FeatureCollection.fromFeatures(featureList));
-    map.getStyle().addSource(geoJsonSource);
-    LineLayer lineLayer = new LineLayer("pre-matched-layer-id", "pre-matched-source-id");
-    lineLayer.setProperties(
-      lineColor(Color.parseColor("#8a8acb")),
-      lineWidth(6f),
-      lineOpacity(1f)
-    );
-    map.getStyle().addLayer(lineLayer);
   }
 
-  private void drawMapMatched(List<Point> coordinates) {
+  private void drawBeforeMapMatching(Feature feature) {
+    Style style = map.getStyle();
+    if (style != null) {
+      style.addSource(new GeoJsonSource("pre-matched-source-id", feature));
+      style.addLayer(new LineLayer("pre-matched-layer-id", "pre-matched-source-id").withProperties(
+        lineColor(ColorUtils.colorToRgbaString(Color.parseColor("#8a8acb"))),
+        lineWidth(6f),
+        lineOpacity(1f)
+      ));
+    } else {
+      throw new IllegalStateException("Style isn't ready yet.");
+    }
+  }
+
+  private void requestMapMatched(Feature feature) {
+    List<Point> points = ((LineString) Objects.requireNonNull(feature.geometry())).coordinates();
+
     try {
       // Setup the request using a client.
       MapboxMapMatching client = MapboxMapMatching.builder()
-        .accessToken(Mapbox.getAccessToken())
+        .accessToken(Objects.requireNonNull(Mapbox.getAccessToken()))
         .profile(PROFILE_DRIVING)
-        .coordinates(coordinates)
+        .coordinates(points)
         .build();
 
       // Execute the API call and handle the response.
       client.enqueueCall(new Callback<MapMatchingResponse>() {
         @Override
-        public void onResponse(Call<MapMatchingResponse> call,
-                               Response<MapMatchingResponse> response) {
+        public void onResponse(@NonNull Call<MapMatchingResponse> call,
+                               @NonNull Response<MapMatchingResponse> response) {
           if (response.code() == 200) {
-            GeoJsonSource geoJsonSource = new GeoJsonSource("matched-source-id",
-              Feature.fromGeometry(LineString.fromPolyline(
-                response.body().matchings().get(0).geometry(), PRECISION_6)));
-            map.getStyle().addSource(geoJsonSource);
-            LineLayer matchedLineLayer = new LineLayer("matched-layer-id", "matched-source-id");
-            matchedLineLayer.setProperties(
-              lineColor(Color.parseColor("#3bb2d0")),
-              lineWidth(6f)
-            );
-            map.getStyle().addLayer(matchedLineLayer);
+            drawMapMatched(Objects.requireNonNull(response.body()).matchings());
           } else {
             // If the response code does not response "OK" an error has occurred.
-            Log.e(TAG, "Too many coordinates, profile not found, invalid input, or no match.");
+            Log.e(TAG, "MapboxMapMatching failed with " + response.code());
           }
         }
 
         @Override
         public void onFailure(Call<MapMatchingResponse> call, Throwable throwable) {
-          Log.e(TAG, "MapboxMapMatching error: " + throwable.getMessage());
+          Log.e(TAG, "MapboxMapMatching error: ", throwable);
         }
       });
     } catch (ServicesException servicesException) {
-      Log.e(TAG, "MapboxMapMatching error: " + servicesException.getMessage());
-      servicesException.printStackTrace();
+      Log.e(TAG, "MapboxMapMatching error: ", servicesException);
+    }
+  }
+
+  private void drawMapMatched(@NonNull List<MapMatchingMatching> matchings) {
+    Style style = map.getStyle();
+    if (style != null && !matchings.isEmpty()) {
+      style.addSource(new GeoJsonSource("matched-source-id", Feature.fromGeometry(LineString.fromPolyline(
+        Objects.requireNonNull(matchings.get(0).geometry()), PRECISION_6)))
+      );
+      style.addLayer(new LineLayer("matched-layer-id", "matched-source-id")
+        .withProperties(
+          lineColor(ColorUtils.colorToRgbaString(Color.parseColor("#3bb2d0"))),
+          lineWidth(6f))
+      );
     }
   }
 }
