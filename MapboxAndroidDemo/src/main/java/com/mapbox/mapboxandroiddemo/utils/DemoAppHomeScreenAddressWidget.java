1fd47018ef37c488432645a7b2cb2d7b0afa1c08
langsmith <langstonlmcs@gmail.com>
2019-01-30 10:46:49 -0800

    refactoring

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/utils/DemoAppHomeScreenAddressWidget.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/utils/DemoAppHomeScreenAddressWidget.java
@@ -6,13 +6,15 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.location.Location;
+import android.support.annotation.NonNull;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
 import com.mapbox.android.core.location.LocationEngine;
-import com.mapbox.android.core.location.LocationEngineListener;
-import com.mapbox.android.core.location.LocationEnginePriority;
+import com.mapbox.android.core.location.LocationEngineCallback;
 import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.location.LocationEngineRequest;
+import com.mapbox.android.core.location.LocationEngineResult;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.api.geocoding.v5.MapboxGeocoding;
@@ -21,6 +23,7 @@ import com.mapbox.api.geocoding.v5.models.GeocodingResponse;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxandroiddemo.R;
 
+import java.lang.ref.WeakReference;
 import java.util.List;
 
 import retrofit2.Call;
@@ -28,25 +31,24 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import timber.log.Timber;
 
+import static android.os.Looper.getMainLooper;
+
 /**
  * Use the Mapbox Java SDK to make a reverse geocode search with the device's current coordinates. Display
  * the address in an Android home screen widget.
  */
 public class DemoAppHomeScreenAddressWidget extends AppWidgetProvider implements
-  LocationEngineListener, PermissionsListener {
+  PermissionsListener {
 
-  private PermissionsManager permissionsManager;
-  private LocationEngine locationEngine;
   private Context context;
-  private Location lastLocation;
-  private AppWidgetManager appWidgetManager;
-  private int singleWidgetId;
+  private LocationChangeListeningActivityLocationCallback callback;
+  private static final long DEFAULT_INTERVAL_IN_MILLISECONDS = 1000L;
+  private static final long DEFAULT_MAX_WAIT_TIME = DEFAULT_INTERVAL_IN_MILLISECONDS * 5;
 
   @Override
   public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-    this.singleWidgetId = appWidgetIds[0];
-    this.appWidgetManager = appWidgetManager;
     this.context = context;
+    this.callback = new LocationChangeListeningActivityLocationCallback(context, appWidgetManager, appWidgetIds[0]);
     checkPermissions(context);
   }
 
@@ -76,7 +78,7 @@ public class DemoAppHomeScreenAddressWidget extends AppWidgetProvider implements
         // Create a location engine instance
         initializeLocationEngine(context);
       } else {
-        permissionsManager = new PermissionsManager(this);
+        PermissionsManager permissionsManager = new PermissionsManager(this);
         permissionsManager.requestLocationPermissions(getActivity(context));
       }
     } catch (NullPointerException nullPointerException) {
@@ -91,14 +93,13 @@ public class DemoAppHomeScreenAddressWidget extends AppWidgetProvider implements
    */
   @SuppressWarnings( {"MissingPermission"})
   private void initializeLocationEngine(Context context) {
-    locationEngine = new LocationEngineProvider(context).obtainBestLocationEngineAvailable();
-    locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
-    locationEngine.addLocationEngineListener(this);
-    locationEngine.activate();
-    lastLocation = locationEngine.getLastLocation();
-    if (lastLocation != null) {
-      getPlaceNameDataFromReverseGeocode(lastLocation, context);
-    }
+    LocationEngine locationEngine = LocationEngineProvider.getBestLocationEngine(context);
+    LocationEngineRequest request = new LocationEngineRequest.Builder(DEFAULT_INTERVAL_IN_MILLISECONDS)
+      .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
+      .setMaxWaitTime(DEFAULT_MAX_WAIT_TIME).build();
+
+    locationEngine.requestLocationUpdates(request, callback, getMainLooper());
+    locationEngine.getLastLocation(callback);
   }
 
   public Activity getActivity(Context context) {
@@ -114,86 +115,116 @@ public class DemoAppHomeScreenAddressWidget extends AppWidgetProvider implements
     return null;
   }
 
-  private void getPlaceNameDataFromReverseGeocode(Location currentDeviceLocation, final Context finalContext) {
-    // Build a Mapbox reverse geocode request
-    MapboxGeocoding reverseGeocode = MapboxGeocoding.builder()
-      .accessToken(finalContext.getString(R.string.access_token))
-      .query(Point.fromLngLat(currentDeviceLocation.getLongitude(), currentDeviceLocation.getLatitude()))
-      .build();
-
-    reverseGeocode.enqueueCall(new Callback<GeocodingResponse>() {
-      @Override
-      public void onResponse(Call<GeocodingResponse> call, Response<GeocodingResponse> response) {
-        try {
-          List<CarmenFeature> carmenFeatureList = response.body().features();
-
-          String textForWidgetTextView;
-
-          // Check that the reverse geocoding response has a place name to display
-          if (carmenFeatureList.size() > 0 && !carmenFeatureList.get(0).placeName().isEmpty()) {
-
-            // Parse through the response to get the place name
-            textForWidgetTextView = carmenFeatureList.get(0).placeName();
-
-            // Construct the RemoteViews object
-            RemoteViews views = new RemoteViews(finalContext.getPackageName(), R.layout.demo_app_home_screen_widget);
-            views.setTextViewText(R.id.device_location_textview, textForWidgetTextView);
-
-            // Instruct the widget manager to update the widget
-            appWidgetManager.updateAppWidget(singleWidgetId, views);
-
-          } else {
-            textForWidgetTextView = context.getString(R.string.no_place_name_for_home_screen_widget);
-            Timber.d("onResponse: no place name");
-          }
-
-          // Construct the RemoteViews object
-          RemoteViews views = new RemoteViews(finalContext.getPackageName(), R.layout.demo_app_home_screen_widget);
-          views.setTextViewText(R.id.device_location_textview, textForWidgetTextView);
-
-          // Instruct the widget manager to update the widget
-          appWidgetManager.updateAppWidget(singleWidgetId, views);
-
-
-        } catch (NullPointerException exception) {
-          Timber.d("onResponse: exception = %s", exception);
-        }
-
-      }
-
-      @Override
-      public void onFailure(Call<GeocodingResponse> call, Throwable throwable) {
-        Timber.d("onFailure: geocoding failure");
-        Toast.makeText(context, R.string.reverse_geocode_failure, Toast.LENGTH_LONG).show();
-        throwable.printStackTrace();
-      }
-    });
-  }
-
   @Override
   public void onDisabled(Context context) {
     // Add message/UI here if you'd like
   }
 
-  @Override
-  @SuppressWarnings( {"MissingPermission"})
-  public void onConnected() {
-    // Add message/UI here if you'd like
-    locationEngine.requestLocationUpdates();
-  }
-
-  @Override
-  public void onLocationChanged(Location location) {
-    if (location != null) {
-      getPlaceNameDataFromReverseGeocode(location, context);
-    } else {
-      locationEngine.addLocationEngineListener(this);
-    }
-  }
 
   @Override
   public void onExplanationNeeded(List<String> permissionsToExplain) {
     // Add explanation here if you'd like
     Toast.makeText(context, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+  }
+
+  private static class LocationChangeListeningActivityLocationCallback
+    implements LocationEngineCallback<LocationEngineResult> {
+
+    private final WeakReference<Context> activityWeakReference;
+    private AppWidgetManager appWidgetManager;
+    private int singleWidgetId;
+
+    LocationChangeListeningActivityLocationCallback(Context context, AppWidgetManager appWidgetManager,
+                                                    int singleWidgetId) {
+      this.activityWeakReference = new WeakReference<>(context);
+      this.appWidgetManager = appWidgetManager;
+      this.singleWidgetId = singleWidgetId;
+    }
+
+    /**
+     * The LocationEngineCallback interface's method which fires when the device's location has changed.
+     *
+     * @param result the LocationEngineResult object which has the last known location within it.
+     */
+    @Override
+    public void onSuccess(LocationEngineResult result) {
+      final Context context = activityWeakReference.get();
+
+      if (context != null) {
+        Location lastKnownLocation = result.getLastLocation();
+
+        if (lastKnownLocation == null) {
+          return;
+        } else {
+          // Build a Mapbox reverse geocode request
+          MapboxGeocoding reverseGeocode = MapboxGeocoding.builder()
+            .accessToken(context.getString(R.string.access_token))
+            .query(Point.fromLngLat(lastKnownLocation.getLongitude(), lastKnownLocation.getLatitude()))
+            .build();
+
+          reverseGeocode.enqueueCall(new Callback<GeocodingResponse>() {
+            @Override
+            public void onResponse(Call<GeocodingResponse> call, Response<GeocodingResponse> response) {
+              try {
+                List<CarmenFeature> carmenFeatureList = response.body().features();
+
+                String textForWidgetTextView;
+
+                // Check that the reverse geocoding response has a place name to display
+                if (carmenFeatureList.size() > 0 && !carmenFeatureList.get(0).placeName().isEmpty()) {
+
+                  // Parse through the response to get the place name
+                  textForWidgetTextView = carmenFeatureList.get(0).placeName();
+
+                  // Construct the RemoteViews object
+                  RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.demo_app_home_screen_widget);
+                  views.setTextViewText(R.id.device_location_textview, textForWidgetTextView);
+
+                  // Instruct the widget manager to update the widget
+                  appWidgetManager.updateAppWidget(singleWidgetId, views);
+
+                } else {
+                  textForWidgetTextView = context.getString(R.string.no_place_name_for_home_screen_widget);
+                  Timber.d("onResponse: no place name");
+                }
+
+                // Construct the RemoteViews object
+                RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.demo_app_home_screen_widget);
+                views.setTextViewText(R.id.device_location_textview, textForWidgetTextView);
+
+                // Instruct the widget manager to update the widget
+                appWidgetManager.updateAppWidget(singleWidgetId, views);
+
+
+              } catch (NullPointerException exception) {
+                Timber.d("onResponse: exception = %s", exception);
+              }
+
+            }
+
+            @Override
+            public void onFailure(Call<GeocodingResponse> call, Throwable throwable) {
+              Timber.d("onFailure: geocoding failure");
+              Toast.makeText(context, R.string.reverse_geocode_failure, Toast.LENGTH_LONG).show();
+              throwable.printStackTrace();
+            }
+          });
+        }
+      }
+    }
+
+    /**
+     * The LocationEngineCallback interface's method which fires when the device's location can not be captured
+     *
+     * @param exception the exception message
+     */
+    @Override
+    public void onFailure(@NonNull Exception exception) {
+      Context context = activityWeakReference.get();
+      if (context != null) {
+        Toast.makeText(context, exception.getLocalizedMessage(),
+          Toast.LENGTH_SHORT).show();
+      }
+    }
   }
 }
