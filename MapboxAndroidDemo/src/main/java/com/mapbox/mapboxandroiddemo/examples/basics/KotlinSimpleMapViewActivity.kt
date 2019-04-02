fcefb0abe712a1b7e1a9897f7c55e1113dedb141
Sam Fader <sam.fader@mapbox.com>
2019-04-02 15:58:39 -0700

    recursive idle listener

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/basics/KotlinSimpleMapViewActivity.kt
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/basics/KotlinSimpleMapViewActivity.kt
@@ -2,68 +2,85 @@ package com.mapbox.mapboxandroiddemo.examples.basics
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.mapbox.mapboxandroiddemo.R
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
 import kotlinx.android.synthetic.main.activity_basic_simple_kotlin.*
+import timber.log.Timber
 
 /**
- * The most basic example of adding a map to an activity with Kotlin code
+ * Test activity showcasing the Snapshot API to create and display a bitmap of the current shown Map.
  */
-class SimpleMapViewActivityKotlin : AppCompatActivity() {
+class SimpleMapViewActivityKotlin : AppCompatActivity(), OnMapReadyCallback {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var mapboxMap: MapboxMap
 
-        // Mapbox access token is configured here. This needs to be called either in your application
-        // object or in the same activity which contains the mapview.
-        Mapbox.getInstance(this, getString(R.string.access_token))
-
-        // This contains the MapView in XML and needs to be called after the access token is configured.
-        setContentView(R.layout.activity_basic_simple_kotlin)
-        mapView?.onCreate(savedInstanceState)
-        mapView?.getMapAsync { mapboxMap ->
-            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
-
-                // Map is set up and the style has loaded. Now you can add data or make other map adjustments.
-
+    private val idleListener = object : MapView.OnDidBecomeIdleListener {
+        override fun onDidBecomeIdle() {
+            mapView.removeOnDidBecomeIdleListener(this)
+            Log.v("idle", "Idle listener removed")
+            mapboxMap.snapshot { snapshot ->
+                imageView.setImageBitmap(snapshot)
+                mapView.addOnDidBecomeIdleListener(this)
+                Log.v("idle", "Idle listener added")
             }
         }
     }
 
-    // Add the mapView lifecycle to the activity's lifecycle methods
-    public override fun onResume() {
-        super.onResume()
-        mapView?.onResume()
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Mapbox.getInstance(this, getString(R.string.access_token));
+        setContentView(R.layout.activity_basic_simple_kotlin)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+    }
+
+
+    override fun onMapReady(map: MapboxMap) {
+        mapboxMap = map
+        mapboxMap.setStyle(Style.Builder().fromUrl(Style.OUTDOORS)) { mapView.addOnDidBecomeIdleListener(idleListener) }
     }
 
     override fun onStart() {
         super.onStart()
-        mapView?.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapboxMap.snapshot {
+            Timber.e("Regression test for https://github.com/mapbox/mapbox-gl-native/pull/11358")
+        }
+        mapView.onPause()
     }
 
     override fun onStop() {
         super.onStop()
-        mapView?.onStop()
+        mapView.onStop()
     }
 
-    public override fun onPause() {
-        super.onPause()
-        mapView?.onPause()
+    public override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
-        mapView?.onLowMemory()
+        mapView.onLowMemory()
     }
 
-    override fun onDestroy() {
+    public override fun onDestroy() {
         super.onDestroy()
-        mapView?.onDestroy()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView?.onSaveInstanceState(outState)
+        mapView.removeOnDidBecomeIdleListener(idleListener)
+        mapView.onDestroy()
     }
 }
