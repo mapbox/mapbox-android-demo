4b9dc383d9070a11d197be8dfa0d68e9880bc321
Langston Smith <langston.smith@mapbox.com>
2019-05-17 09:54:54 -0700

    Adjusted HomeScreenWidgetActivity class' location and its javadocs (#1031)

---
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/labs/HomeScreenWidgetActivity.java
@@ -0,0 +1,24 @@
+package com.mapbox.mapboxandroiddemo.examples.labs;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mapbox.mapboxandroiddemo.R;
+
+/**
+ * A placeholder activity and XML layout, which describe instruction on installing and viewing
+ * the demo app's home screen widget. The widget retrieves the device's location,
+ * makes a geocoding request to the Mapbox Geocoding API, and displays the real-world
+ * address that's associated with the device's location.
+ *
+ * Visit the {@link com.mapbox.mapboxandroiddemo.utils.DemoAppHomeScreenAddressWidget}
+ * class to see the internal workings of the widget.
+ */
+public class HomeScreenWidgetActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_homescreen_geocoding_widget);
+  }
+}
