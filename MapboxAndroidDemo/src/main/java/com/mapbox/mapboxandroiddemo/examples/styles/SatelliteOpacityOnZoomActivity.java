251f0166bb22916f08874d9736dc222b716b06cd
langsmith <langstonlmcs@gmail.com>
2019-11-27 11:42:04 -0800

    initial additions

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/styles/SatelliteOpacityOnZoomActivity.java
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/styles/SatelliteOpacityOnZoomActivity.java
@@ -1,6 +1,9 @@
 package com.mapbox.mapboxandroiddemo.examples.styles;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +15,9 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.expressions.Expression;
+import com.mapbox.mapboxsdk.style.layers.Layer;
+import com.mapbox.mapboxsdk.style.layers.PropertyValue;
 import com.mapbox.mapboxsdk.style.layers.RasterLayer;
 import com.mapbox.mapboxsdk.style.sources.RasterSource;
 
@@ -29,7 +35,21 @@ import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.rasterOpacity;
 public class SatelliteOpacityOnZoomActivity extends AppCompatActivity implements
   OnMapReadyCallback {
 
+  private static final String SATELLITE_RASTER_SOURCE_ID = "SATELLITE_RASTER_SOURCE_ID";
+  private static final String SATELLITE_RASTER_LAYER_ID = "SATELLITE_RASTER_LAYER_ID";
   private MapView mapView;
+  private MapboxMap mapboxMap;
+  private boolean satelliteSetToReveal = false;
+
+  private PropertyValue<Expression> expressionRevealingSatellite = rasterOpacity(interpolate(linear(), zoom(),
+    stop(15, 0),
+    stop(18, 1)
+  ));
+
+  private PropertyValue<Expression> expressionRevealingMapboxStreets = rasterOpacity(interpolate(linear(), zoom(),
+    stop(12, 1),
+    stop(16, 0)
+  ));
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -49,28 +69,51 @@ public class SatelliteOpacityOnZoomActivity extends AppCompatActivity implements
 
   @Override
   public void onMapReady(@NonNull final MapboxMap mapboxMap) {
-
     mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
       @Override
       public void onStyleLoaded(@NonNull Style style) {
+
+        SatelliteOpacityOnZoomActivity.this.mapboxMap = mapboxMap;
+
         // Create a data source for the satellite raster image and add the source to the map
-        style.addSource(new RasterSource("SATELLITE_RASTER_SOURCE_ID",
+        style.addSource(new RasterSource(SATELLITE_RASTER_SOURCE_ID,
           "mapbox://mapbox.satellite", 512));
 
         // Create a new map layer for the satellite raster images and add the satellite layer to the map.
         // Use runtime styling to adjust the satellite layer's opacity based on the map camera's zoom level
         style.addLayer(
-          new RasterLayer("SATELLITE_RASTER_LAYER_ID", "SATELLITE_RASTER_SOURCE_ID").withProperties(
-          rasterOpacity(interpolate(linear(), zoom(),
-            stop(15, 0),
-            stop(18, 1)
-          ))));
+          new RasterLayer(SATELLITE_RASTER_LAYER_ID, SATELLITE_RASTER_SOURCE_ID).withProperties(
+            expressionRevealingSatellite));
 
         // Create a new camera position and animate the map camera to show the fade in/out UI of the satellite layer
         mapboxMap.animateCamera(
           CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder()
             .zoom(19)
             .build()), 9000);
+
+        findViewById(R.id.swap_streets_and_satellite_order_toggle_fab).setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+            swapStreetsAndSatelliteOrder();
+            Toast.makeText(SatelliteOpacityOnZoomActivity.this,
+              R.string.zoom_in_and_out_instruction, Toast.LENGTH_SHORT).show();
+          }
+        });
+      }
+    });
+  }
+
+  private void swapStreetsAndSatelliteOrder() {
+    mapboxMap.getStyle(new Style.OnStyleLoaded() {
+      @Override
+      public void onStyleLoaded(@NonNull Style style) {
+        Layer satelliteLayer = style.getLayer(SATELLITE_RASTER_LAYER_ID);
+        if (satelliteLayer != null) {
+          satelliteLayer.setProperties(
+            satelliteSetToReveal ? expressionRevealingSatellite : expressionRevealingMapboxStreets
+          );
+        }
+        satelliteSetToReveal = !satelliteSetToReveal;
       }
     });
   }
